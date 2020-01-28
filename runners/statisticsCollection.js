46e34578cfe742c41f9f220f1f290fbdca42792a
James Mackay <sirjamesmackay@gmail.com>
2020-01-28 19:17:23 +0000

    dashboard farm statistics load

--- runners/statisticsCollection.js
+++ runners/statisticsCollection.js
@@ -1,5 +1,6 @@
 const FarmStatistics = require("../models/FarmStatistics.js");
 const Printers = require("../models/Printer.js");
+const History = require("../models/History.js");
 const _ = require("lodash");
 
 class StatisticsCollection {
@@ -51,9 +52,9 @@ class StatisticsCollection {
               name: printer.settingsApperance.name,
               progress: Math.floor(printer.progress.completion),
               progressColour: "success"
-            })
-            printTimeRemaining.push(printer.progress.printTimeLeft)
-            printTimeElapsed.push(printer.progress.printTime)
+            });
+            printTimeRemaining.push(printer.progress.printTimeLeft);
+            printTimeElapsed.push(printer.progress.printTime);
           }
           if (printer.stateColour.category === "Active") {
             active.push(printer.index);
@@ -67,10 +68,10 @@ class StatisticsCollection {
               name: printer.settingsApperance.name,
               progress: Math.floor(printer.progress.completion),
               progressColour: "warning"
-            })
-            printTimeEstimate.push(printer.job.estimatedPrintTime)
-            printTimeRemaining.push(printer.progress.printTimeLeft)
-            printTimeElapsed.push(printer.progress.printTime)
+            });
+            printTimeEstimate.push(printer.job.estimatedPrintTime);
+            printTimeRemaining.push(printer.progress.printTimeLeft);
+            printTimeElapsed.push(printer.progress.printTime);
           }
           if (printer.stateColour.category === "Idle") {
             idle.push(printer.index);
@@ -90,39 +91,64 @@ class StatisticsCollection {
         farmInfo.active = active.length;
         farmInfo.idle = idle.length;
         farmInfo.offline = offline.length;
-        farmInfo.activeToolT = Math.round(toolA.reduce((a, b) => a + b, 0) * 10)/10;
-        farmInfo.activeToolA = Math.round(toolT.reduce((a, b) => a + b, 0) * 10)/10;
-        farmInfo.activeBedA = Math.round(bedA.reduce((a, b) => a + b, 0) * 10)/10;
-        farmInfo.activeBedT = Math.round(bedT.reduce((a, b) => a + b, 0) * 10)/10;
+        farmInfo.activeToolT =
+          Math.round(toolA.reduce((a, b) => a + b, 0) * 10) / 10;
+        farmInfo.activeToolA =
+          Math.round(toolT.reduce((a, b) => a + b, 0) * 10) / 10;
+        farmInfo.activeBedA =
+          Math.round(bedA.reduce((a, b) => a + b, 0) * 10) / 10;
+        farmInfo.activeBedT =
+          Math.round(bedT.reduce((a, b) => a + b, 0) * 10) / 10;
         let actProg = progress.reduce((a, b) => a + b, 0);
-        farmInfo.farmProgress = Math.floor(actProg / progress.length)
-        if(farmInfo.farmProgress === 100){
-          farmInfo.farmProgressColour = "success"
-        }else{
-          farmInfo.farmProgressColour = "warning"
+        farmInfo.farmProgress = Math.floor(actProg / progress.length);
+        if (farmInfo.farmProgress === 100) {
+          farmInfo.farmProgressColour = "success";
+        } else {
+          farmInfo.farmProgressColour = "warning";
         }
         farmInfo.currentOperations = currentOperations;
         farmInfo.totalElapsedTime = printTimeElapsed.reduce((a, b) => a + b, 0);
-        farmInfo.totalRemainingTime = printTimeRemaining.reduce((a, b) => a + b, 0);
-        farmInfo.totalEstimateTime = printTimeEstimate.reduce((a, b) => a + b, 0);
-        farmInfo.avgElapsedTime =  farmInfo.totalElapsedTime / printTimeElapsed.length;
-        farmInfo.avgRemainingTime = farmInfo.totalRemainingTime / printTimeRemaining.length;
-        farmInfo.avgEstimateTime = farmInfo.totalEstimateTime / printTimeEstimate.length;
+        farmInfo.totalRemainingTime = printTimeRemaining.reduce(
+          (a, b) => a + b,
+          0
+        );
+        farmInfo.totalEstimateTime = printTimeEstimate.reduce(
+          (a, b) => a + b,
+          0
+        );
+        farmInfo.avgElapsedTime =
+          farmInfo.totalElapsedTime / printTimeElapsed.length;
+        farmInfo.avgRemainingTime =
+          farmInfo.totalRemainingTime / printTimeRemaining.length;
+        farmInfo.avgEstimateTime =
+          farmInfo.totalEstimateTime / printTimeEstimate.length;
 
         farmStats[0].farmInfo = farmInfo;
 
         //Farm Statistics
-        let currentDate = new Date();
-        octofarmStatistics.activeHours = currentDate.getTime() - farmStats[0].farmStart.getTime();
-        octofarmStatistics.activeHours = octofarmStatistics.activeHours / 1000;
-        octofarmStatistics.activeHours = octofarmStatistics.activeHours - octofarmStatistics.idleHours
-        octofarmStatistics.idleHours = 0; //Can't get yet due to no history....
-        if(octofarmStatistics.idleHours === 0){
-          octofarmStatistics.activePercent = 100;
-        }else{
-          octofarmStatistics.activePercent =  Math.floor(octofarmStatistics.idleHours / octofarmStatistics.activeHours * 100);
-        }
+        let history = await History.find({});
+        let printTimes = [];
+        history.forEach(print => {
+          printTimes.push(print.printHistory.printTime);
+        });
 
+        let printTimesTotal = printTimes.reduce((a, b) => a + b, 0);
+
+        let currentDate = new Date();
+        octofarmStatistics.activeHours = printTimesTotal;
+        octofarmStatistics.idleHours =
+          currentDate.getTime() - farmStats[0].farmStart.getTime();
+        octofarmStatistics.idleHours = octofarmStatistics.idleHours / 1000;
+        octofarmStatistics.idleHours =
+          octofarmStatistics.idleHours - octofarmStatistics.activeHours;
+        if (octofarmStatistics.idleHours === 0) {
+          octofarmStatistics.activePercent = 100;
+        } else {
+          octofarmStatistics.activePercent = Math.floor(
+            (octofarmStatistics.activeHours / octofarmStatistics.idleHours) *
+              100
+          );
+        }
 
         let storageFree = [];
         let storageTotal = [];
@@ -134,28 +160,30 @@ class StatisticsCollection {
             ip: printer.ip,
             index: printer.index,
             storage: printer.storage
+          };
+          devices.push(device);
+          if (typeof printer.fileList.files != "undefined") {
+            printer.fileList.files.forEach(file => {
+              fileSizes.push(file.size);
+            });
           }
-            devices.push(device);
-            if(typeof printer.fileList.files != 'undefined'){
-              printer.fileList.files.forEach(file => {
-                fileSizes.push(file.size)
-              })
-            }
-        })
+        });
 
-        let uniqueDevices = _.uniqBy(devices, "ip")
+        let uniqueDevices = _.uniqBy(devices, "ip");
 
         uniqueDevices.forEach(device => {
           storageFree.push(device.storage.free);
           storageTotal.push(device.storage.total);
-        })
+        });
 
         let storageFreeTotal = storageFree.reduce((a, b) => a + b, 0);
         let storageTotalTotal = storageTotal.reduce((a, b) => a + b, 0);
 
         octofarmStatistics.storageUsed = storageTotalTotal - storageFreeTotal;
         octofarmStatistics.storageRemain = storageFreeTotal;
-        octofarmStatistics.storagePercent = Math.floor(octofarmStatistics.storageUsed / storageTotalTotal * 100);
+        octofarmStatistics.storagePercent = Math.floor(
+          (octofarmStatistics.storageUsed / storageTotalTotal) * 100
+        );
 
         octofarmStatistics.largestFile = Math.max(...fileSizes);
         octofarmStatistics.smallestFile = Math.min(...fileSizes);
@@ -189,7 +217,7 @@ class StatisticsCollection {
     };
     return farmInfo;
   }
-  static blankFarmStatistics(){
+  static blankFarmStatistics() {
     let octofarmStatistics = {
       activeHours: 0,
       idleHours: 0,
@@ -199,12 +227,10 @@ class StatisticsCollection {
       storageRemain: 0,
       largestFile: 0,
       smallestFile: 0
-    }
+    };
     return octofarmStatistics;
   }
-  static blankPrintStatistics(){
-    
-  }
+  static blankPrintStatistics() {}
 }
 
 module.exports = {
