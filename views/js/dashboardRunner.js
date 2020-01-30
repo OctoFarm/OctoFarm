fc3538f5026a48c300da32a43551890846ec19ac
James <james.mackay@vexica.com>
2020-01-30 12:01:30 +0000

    current op progress styles wasn't updating

--- views/js/dashboardRunner.js
+++ views/js/dashboardRunner.js
@@ -1,6 +1,7 @@
 import Client from "./lib/octofarm.js";
 import UI from "./lib/functions/ui.js";
 import Calc from "./lib/functions/calc.js";
+import currentOperations from "./lib/modules/currentOperations.js";
 
 setInterval(function() {
   Client.get("client/dash/get")
@@ -19,7 +20,10 @@ setInterval(function() {
   
         dashUpdate.systemInformation(systemInfo);
         dashUpdate.printers(printers);
-        dashUpdate.currentOperations(farmInfo);
+        currentOperations(farmInfo);
+        dashUpdate.farmInformation(farmInfo);
+        //dashUpdate.FarmStatistics
+        //dashUpdate.PrintStatistics
       }
     })
     .catch(err => {
@@ -97,58 +101,8 @@ class dashUpdate {
       }
     });
   }
-  static currentOperations(farmInfo) {
-    document.getElementById("completeCount").innerHTML =
-      "Complete: " + farmInfo.complete;
-    document.getElementById("idleCount").innerHTML = "Idle: " + farmInfo.idle;
-    document.getElementById("activeCount").innerHTML =
-      "Active: " + farmInfo.active;
-    document.getElementById("offlineCount").innerHTML =
-      "Offline: " + farmInfo.offline;
-    farmInfo.currentOperations = _.orderBy(
-      farmInfo.currentOperations,
-      ["progress"],
-      ["desc"]
-    );
-    farmInfo.currentOperations.forEach((current, index) => {
-      //check if exists, create if not....
-      if (document.getElementById("currentOpCard-" + current.index)) {
-        let progress = document.getElementById(
-          "currentProgress-" + current.index
-        );
-        progress.style.width = current.progress;
-        progress.innerHTML = current.progress + "%";
-        progress.className = `progress-bar progress-bar-striped bg-${current.progressColour}`;
-      } else {
-        document.getElementById("currentOperationsBody").insertAdjacentHTML(
-          "beforeend",
-          `
-                <div id="currentOpCard-${current.index}"
-                class="card card-block text-white bg-secondary d-inline-block"
-                style="min-width: 200px; height:65px;"
-              >
-                  <div class="card-header pb-1 pt-1 pl-2 pr-2">${current.index}. ${current.name}</div>
-                  <div class="card-body  pb-0 pt-2 pl-2 pr-2">
-                    <div class="progress">
-                      <div id="currentProgress-${current.index}"
-                        class="progress-bar progress-bar-striped bg-${current.progressColour}"
-                        role="progressbar"
-                        style="width: ${current.progress}%"
-                        aria-valuenow="${current.progress}"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                      >
-                      ${current.progress}%
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                `
-        );
-      }
-      document.getElementById(
-        "currentOpCard-" + current.index
-      ).style.order = index;
-    });
+
+  static farmInformation(farmInformation){
+      
   }
 }
---
+++ views/js/lib/modules/currentOperations.js
@@ -0,0 +1,55 @@
+export default function currentOperations(farmInfo) {
+    console.log("UPDATE OPS")
+    document.getElementById("completeCount").innerHTML =
+      "Complete: " + farmInfo.complete;
+    document.getElementById("idleCount").innerHTML = "Idle: " + farmInfo.idle;
+    document.getElementById("activeCount").innerHTML =
+      "Active: " + farmInfo.active;
+    document.getElementById("offlineCount").innerHTML =
+      "Offline: " + farmInfo.offline;
+    farmInfo.currentOperations = _.orderBy(
+      farmInfo.currentOperations,
+      ["progress"],
+      ["desc"]
+    );
+    farmInfo.currentOperations.forEach((current, index) => {
+      //check if exists, create if not....
+      if (document.getElementById("currentOpCard-" + current.index)) {
+        let progress = document.getElementById(
+          "currentProgress-" + current.index
+        );
+        progress.style = `width: ${current.progress}%`;
+        progress.innerHTML = current.progress + "%";
+        progress.className = `progress-bar progress-bar-striped bg-${current.progressColour}`;
+      } else {
+        document.getElementById("currentOperationsBody").insertAdjacentHTML(
+          "beforeend",
+          `
+                <div id="currentOpCard-${current.index}"
+                class="card card-block text-white bg-secondary d-inline-block"
+                style="min-width: 200px; height:65px;"
+              >
+                  <div class="card-header pb-1 pt-1 pl-2 pr-2">${current.index}. ${current.name}</div>
+                  <div class="card-body  pb-0 pt-2 pl-2 pr-2">
+                    <div class="progress">
+                      <div id="currentProgress-${current.index}"
+                        class="progress-bar progress-bar-striped bg-${current.progressColour}"
+                        role="progressbar"
+                        style="width: ${current.progress}%"
+                        aria-valuenow="${current.progress}"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                      ${current.progress}%
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                `
+        );
+      }
+      document.getElementById(
+        "currentOpCard-" + current.index
+      ).style.order = index;
+    });
+  }
