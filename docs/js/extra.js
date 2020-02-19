2d9d8c06ccbd5930f0bb1b40161eb2476b21b540
James Mackay <sirjamesmackay@gmail.com>
2020-02-19 18:28:12 +0000

    remove console.log

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,6 +1,4 @@
 $(document).on("click", ".navbar-nav", function(e) {
-  console.log("clicked");
-  console.log(e.target);
   if ($(e.target).is("a")) {
     $(".navbar-collapse").collapse("hide");
   }
