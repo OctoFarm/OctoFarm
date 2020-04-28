71fe5333aade03c513c518bbd2342e081a6faaee
notexpectedyet <online@notexpectedyet.com>
2020-04-28 08:08:45 +0100

    New Timeout Option: Api Retry for retrying the API connection when trying to get a printer online

--- serverConfig/timeout.js
+++ serverConfig/timeout.js
@@ -2,6 +2,7 @@ module.exports = {
     webSocketRetry: 5000,
     apiTimeout: 1000,
     apiRetryCutoff: 10000,
+    apiRetry: 300000,
 };
 //Web Socket Retry: How often to poll for an offline websocket connection when lost.
 //API Timeout: The timeout clause on the API, increase if your printers (mainly prusa) are having issues grabbing the initial connection.
