05b3cccf949eded284c86cc7ed6f3f3e0a3d6bef
notexpectedyet <online@notexpectedyet.com>
2020-04-28 08:17:46 +0100

    Added new settings for later use

--- settings/serverSettings.js
+++ settings/serverSettings.js
@@ -3,20 +3,38 @@ const ServerSettingsDB = require("../models/ServerSettings.js");
 class ServerSettings {
   static init() {
     ServerSettingsDB.find({}).then(settings => {
+      //Default Settings
+      let onlinePolling = {
+        seconds: 0.5
+      };
+      let server = {
+        port: 4000,
+        registration: true,
+        loginRequired: true
+      };
+      let timeout = {
+        webSocketRetry: 5000,
+        apiTimeout: 1000,
+        apiRetryCutoff: 10000,
+        apiRetry: 300000
+      }
       if (settings.length < 1) {
-        let onlinePolling = {
-          seconds: 0.5
-        };
-        let offlinePolling = {
-          on: true,
-          seconds: 300000
-        };
         let defaultSystemSettings = new ServerSettingsDB({
           onlinePolling,
-          offlinePolling
+          server,
+          timeout
         });
         defaultSystemSettings.save();
         return "Server settings have been created...";
+      }else{
+        //Server settings exist, but need updating with new ones if they don't exists.
+        if(typeof settings[0].timeout === 'undefined'){
+          settings[0].timeout = timeout;
+        }
+        if(typeof settings[0].server === 'undefined'){
+          settings[0].server = server;
+        }
+        settings[0].save();
       }
     });
     return "Server settings already exist, loaded existing values...";
