3603cff3cc01c506574b669f07996166f4280389
notexpectedyet <online@notexpectedyet.com>
2020-05-09 17:08:33 +0100

    Only multiply idle time by Inactive printers
    Signed-off-by: notexpectedyet <online@notexpectedyet.com>

--- runners/statisticsCollection.js
+++ runners/statisticsCollection.js
@@ -429,10 +429,34 @@ class StatisticsCollection {
         farmPrinters.forEach(printer => {
             activeTimeTotle.push(printer.currentUptime);
         })
+        let offline = []
+        farmPrinters.forEach(printer => {
+            if (typeof printer.stateColour != "undefined") {
+                if (printer.stateColour.category === "Idle") {
+                    offline.push(printer._id);
+                }
+                if (
+                    printer.stateColour.category === "Offline"
+                ) {
+                    offline.push(printer._id);
+                }
+                if (
+                    printer.stateColour.category === "Disconnected"
+                ) {
+                    offline.push(printer._id);
+                }
+                if (
+                    printer.stateColour.category === "Complete"
+                ) {
+                    offline.push(printer._id);
+                }
 
+            }
+        })
         let activeTime = activeTimeTotle.reduce((a, b) => a + b, 0);
         let idleTime  =  dateNow - farmStats[0].farmStart.getTime();
-        idleTime = idleTime * farmPrinters.length
+
+        idleTime = idleTime * offline.length
         let totalHours = activeTime + idleTime;
         octofarmStatistics.activeHours = activeTime;
         octofarmStatistics.idleHours = idleTime;
