b262ca961fd7de5cc0486cfb109b07366b4dfff9
notexpectedyet <online@notexpectedyet.com>
2020-06-10 10:50:46 +0100

    change default db.js uri to localhost
    Signed-off-by: notexpectedyet <online@notexpectedyet.com>

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MongoURI: "mongodb://192.168.1.5:27017/octofarm"
+  MongoURI: "mongodb://localhost:27017/octofarm"
 };
 //Example Local URL: "mongodb://192.168.1.5:27017/octofarm"
 //Example Remote URL: "mongodb+srv://s1mpleman:<YOUR PASSWORD>@cluster0-lgugu.mongodb.net/test?retryWrites=true&w=majority"
