1de1f740f0db979ee92d3766da09f18c778c761a
James Mackay <info@notexpectedyet.com>
2020-06-29 05:51:38 +0100

    added routes for printer manager and dashboard information

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ let serverStart = async function() {
             app.use("/printers", require("./routes/printers", { page: "route" }));
             app.use("/settings", require("./routes/settings", { page: "route" }));
             app.use("/printersInfo", require("./routes/SSE-printersInfo", { page: "route" }));
-            // app.use("/dashboardInfo", require("./routes/SSE-dashboard", { page: "route" }));
+            app.use("/dashboardInfo", require("./routes/SSE-dashboard", { page: "route" }));
             // app.use("/monitoringInfo", require("./routes/SSE-monitoring", { page: "route" }));
             app.use("/filament", require("./routes/filament", { page: "route" }));
             app.use("/history", require("./routes/history", { page: "route" }));
