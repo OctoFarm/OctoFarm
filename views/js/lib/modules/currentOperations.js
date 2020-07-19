b3fdc25288810c305be642b3b88cc1dc356c5718
James Mackay <info@notexpectedyet.com>
2020-07-19 14:30:26 +0100

    current operatiosn view activation add file name to view

--- views/js/lib/modules/currentOperations.js
+++ views/js/lib/modules/currentOperations.js
@@ -1,102 +1,101 @@
-import Calc from "../functions/calc.js";
-import OctoPrintClient from "../octoprint.js";
+import Calc from '../functions/calc.js'
+import OctoPrintClient from '../octoprint.js'
 
-let printers = [];
-let resetFile = function(id){
-  let i = _.findIndex(printers, function(o) { return o._id == id; });
-  OctoPrintClient.file(printers[i], printers[i].currentJob.filePath, "load");
+let printers = []
+const resetFile = function (id) {
+  const i = _.findIndex(printers, function (o) { return o._id == id })
+  OctoPrintClient.file(printers[i], printers[i].currentJob.filePath, 'load')
 }
-let rePrint = function(id){
-    let i = _.findIndex(printers, function(o) { return o._id == id; });
-    OctoPrintClient.file(printers[i], printers[i].currentJob.filePath, "print");
+const rePrint = function (id) {
+  const i = _.findIndex(printers, function (o) { return o._id == id })
+  OctoPrintClient.file(printers[i], printers[i].currentJob.filePath, 'print')
 }
-let currentHarvest = document.querySelectorAll("[id^='currentHarvest-']");
+const currentHarvest = document.querySelectorAll("[id^='currentHarvest-']")
 currentHarvest.forEach(harvest => {
-    harvest.addEventListener("click", e => {
-        let id = harvest.id.split("-");
-        resetFile(id[1]);
-    })
+  harvest.addEventListener('click', e => {
+    const id = harvest.id.split('-')
+    resetFile(id[1])
+  })
 })
-let currentRestartPrint = document.querySelectorAll("[id^='restartCurrentPrint-']");
+const currentRestartPrint = document.querySelectorAll("[id^='restartCurrentPrint-']")
 currentRestartPrint.forEach(harvest => {
-    harvest.addEventListener("click", e => {
-        let id = harvest.id.split("-");
-        rePrint(id[1]);
-    })
+  harvest.addEventListener('click', e => {
+    const id = harvest.id.split('-')
+    rePrint(id[1])
+  })
 })
-export default function currentOperations(
+export default function currentOperations (
   currentOperations,
   currentOperationsCount,
   printerInfo
 ) {
-    printers = printerInfo;
-  if(currentOperations.length === 0){
-    let currentCards = document.querySelectorAll("[id^='currentOpCard-']");
+  printers = printerInfo
+  if (currentOperations.length === 0) {
+    const currentCards = document.querySelectorAll("[id^='currentOpCard-']")
     currentCards.forEach(card => {
-      card.remove();
+      card.remove()
     })
   }
 
- currentOperationsCount = currentOperationsCount;
-  document.getElementById("completeCount").innerHTML =
-    "Complete: " + currentOperationsCount.complete;
-  document.getElementById("idleCount").innerHTML =
-    "Idle: " + currentOperationsCount.idle;
-  document.getElementById("activeCount").innerHTML =
-    "Active: " + currentOperationsCount.active;
-    document.getElementById("disconCount").innerHTML =
-        "Disconnected: " + currentOperationsCount.disconnected;
+  currentOperationsCount = currentOperationsCount
+  document.getElementById('completeCount').innerHTML =
+    'Complete: ' + currentOperationsCount.complete
+  document.getElementById('idleCount').innerHTML =
+    'Idle: ' + currentOperationsCount.idle
+  document.getElementById('activeCount').innerHTML =
+    'Active: ' + currentOperationsCount.active
+  document.getElementById('disconCount').innerHTML =
+        'Disconnected: ' + currentOperationsCount.disconnected
 
-  document.getElementById("offlineCount").innerHTML =
-    "Offline: " + currentOperationsCount.offline;
+  document.getElementById('offlineCount').innerHTML =
+    'Offline: ' + currentOperationsCount.offline
 
-  document.getElementById("farmProgress").innerHTML =
-    currentOperationsCount.farmProgress + "%";
+  document.getElementById('farmProgress').innerHTML =
+    currentOperationsCount.farmProgress + '%'
   document.getElementById(
-    "farmProgress"
-  ).style = `width: ${currentOperationsCount.farmProgress}%`;
+    'farmProgress'
+  ).style = `width: ${currentOperationsCount.farmProgress}%`
   document.getElementById(
-    "farmProgress"
-  ).classList = `progress-bar progress-bar-striped bg-${currentOperationsCount.farmProgressColour}`;
+    'farmProgress'
+  ).classList = `progress-bar progress-bar-striped bg-${currentOperationsCount.farmProgressColour}`
 
-  currentOperations = _.orderBy(currentOperations, ["progress"], ["desc"]);
+  currentOperations = _.orderBy(currentOperations, ['progress'], ['desc'])
 
   currentOperations.forEach((current, index) => {
-    //Generate future time
-    let currentDate = new Date(); 
-    currentDate = currentDate.getTime(); 
-    let futureDateString = new Date(currentDate + current.timeRemaining * 1000).toDateString()
+    // Generate future time
+    let currentDate = new Date()
+    currentDate = currentDate.getTime()
+    const futureDateString = new Date(currentDate + current.timeRemaining * 1000).toDateString()
     let futureTimeString = new Date(currentDate + current.timeRemaining * 1000).toTimeString()
-    futureTimeString = futureTimeString.substring(0, 8);
-    let dateComplete = futureDateString + ": " + futureTimeString;
-    let finishedPrint = `<button id='currentHarvest-${current.index}' type='button' title="Clear your finished print from current operations" class='tag btn btn-success btn-sm mt-0 pt-0 pb-0'>Print Harvested?</button>`;
-    let restartPrint = `<button id='restartCurrentPrint-${current.index}' type='button' title="Restart your current selected print" class='tag btn btn-warning btn-sm mt-0 pt-0 pb-0'>Restart Print</button>`
-    //check if exists, create if not....
-    if (document.getElementById("currentOpCard-" + current.index)) {
-        if(current.progress === 100){
-            document.getElementById("finishedPrint-"+current.index).classList.remove("d-none");
-            document.getElementById("futureDate-"+current.index).classList.add("d-none");
-            document.getElementById("currentRestart-"+current.index).classList.remove("d-none");
-            document.getElementById("currentTime-"+current.index).classList.add("d-none");
-            document.getElementById("currentProgressMain-"+current.index).classList.add("d-none");
-        }else{
-            document.getElementById("finishedPrint-"+current.index).classList.add("d-none");
-            document.getElementById("futureDate-"+current.index).classList.remove("d-none");
-            document.getElementById("currentRestart-"+current.index).classList.add("d-none");
-            document.getElementById("currentTime-"+current.index).classList.remove("d-none");
-            document.getElementById("currentProgressMain-"+current.index).classList.remove("d-none");
-        }
-      let progress = document.getElementById(
-        "currentProgress-" + current.index
-      );
-      document.getElementById("currentTime-"+current.index).innerHTML = Calc.generateTime(current.timeRemaining);
-      progress.style = `width: ${current.progress}%`;
-      progress.innerHTML = current.progress + "%";
-      progress.className = `progress-bar progress-bar-striped bg-${current.progressColour}`;
-
+    futureTimeString = futureTimeString.substring(0, 8)
+    const dateComplete = futureDateString + ': ' + futureTimeString
+    const finishedPrint = `<button id='currentHarvest-${current.index}' type='button' title="Clear your finished print from current operations" class='tag btn btn-success btn-sm mt-0 pt-0 pb-0'>Print Harvested?</button>`
+    const restartPrint = `<button id='restartCurrentPrint-${current.index}' type='button' title="Restart your current selected print" class='tag btn btn-warning btn-sm mt-0 pt-0 pb-0'>Restart Print</button>`
+    // check if exists, create if not....
+    if (document.getElementById('currentOpCard-' + current.index)) {
+      if (current.progress === 100) {
+        document.getElementById('finishedPrint-' + current.index).classList.remove('d-none')
+        document.getElementById('futureDate-' + current.index).classList.add('d-none')
+        document.getElementById('currentRestart-' + current.index).classList.remove('d-none')
+        document.getElementById('currentTime-' + current.index).classList.add('d-none')
+        document.getElementById('currentProgressMain-' + current.index).classList.add('d-none')
+      } else {
+        document.getElementById('finishedPrint-' + current.index).classList.add('d-none')
+        document.getElementById('futureDate-' + current.index).classList.remove('d-none')
+        document.getElementById('currentRestart-' + current.index).classList.add('d-none')
+        document.getElementById('currentTime-' + current.index).classList.remove('d-none')
+        document.getElementById('currentProgressMain-' + current.index).classList.remove('d-none')
+      }
+      const progress = document.getElementById(
+        'currentProgress-' + current.index
+      )
+      document.getElementById('currentTime-' + current.index).innerHTML = Calc.generateTime(current.timeRemaining)
+      progress.style = `width: ${current.progress}%`
+      progress.innerHTML = current.progress + '%'
+      progress.className = `progress-bar progress-bar-striped bg-${current.progressColour}`
     } else {
-      document.getElementById("currentOperationsBody").insertAdjacentHTML(
-        "beforeend",
+      document.getElementById('currentOperationsBody').insertAdjacentHTML(
+        'beforeend',
         `
                 <div id="currentOpCard-${current.index}"
                 class="card card-block text-white bg-secondary d-inline-block"
@@ -105,6 +104,7 @@ export default function currentOperations(
                   <div class="card-header pb-1 pt-1 pl-2 pr-2">
                      ${current.name}</div>
                   <div class="card-body  pb-0 pt-2 pl-2 pr-2">
+                  <h6 id="currentFile-${current.index}" class="pb-0 text-center" style="font-size:0.6rem;"> ${current.fileName} </h6>
                   <h6 id="currentRestart-${current.index}" class="pb-0 text-center d-none" style="font-size:0.6rem;">${restartPrint}</h6>
                   <h6 id="currentTime-${
                     current.index
@@ -130,33 +130,33 @@ export default function currentOperations(
                   </div>
                 </div>
                 `
-      );
-        document.getElementById("currentHarvest-"+current.index).addEventListener("click", e => {
-            let id =  document.getElementById("currentHarvest-"+current.index).id.split("-");
-            resetFile(id[1]);
-        })
-        document.getElementById("restartCurrentPrint-"+current.index).addEventListener("click", e => {
-            let id =  document.getElementById("restartCurrentPrint-"+current.index).id.split("-");
-            rePrint(id[1]);
-        })
+      )
+      document.getElementById('currentHarvest-' + current.index).addEventListener('click', e => {
+        const id = document.getElementById('currentHarvest-' + current.index).id.split('-')
+        resetFile(id[1])
+      })
+      document.getElementById('restartCurrentPrint-' + current.index).addEventListener('click', e => {
+        const id = document.getElementById('restartCurrentPrint-' + current.index).id.split('-')
+        rePrint(id[1])
+      })
     }
 
     document.getElementById(
-      "currentOpCard-" + current.index
-    ).style.order = index;
-    let currentCards = document.querySelectorAll("[id^='currentOpCard-']");
-    let curr = [];
+      'currentOpCard-' + current.index
+    ).style.order = index
+    const currentCards = document.querySelectorAll("[id^='currentOpCard-']")
+    const curr = []
     currentOperations.forEach(cur => {
-      curr.push(cur.index);
-    });
-    let cards = [];
+      curr.push(cur.index)
+    })
+    const cards = []
     currentCards.forEach(card => {
-      let ca = card.id.split("-");
-      cards.push(ca[1]);
-    });
-    let remove = _.difference(cards, curr);
+      const ca = card.id.split('-')
+      cards.push(ca[1])
+    })
+    const remove = _.difference(cards, curr)
     remove.forEach(rem => {
-      document.getElementById("currentOpCard-" + rem).remove();
-    });
-  });
+      document.getElementById('currentOpCard-' + rem).remove()
+    })
+  })
 }
