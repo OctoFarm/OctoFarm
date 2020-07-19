dd40167214fe1a1037afe68687d4eb18363f3894
James Mackay <info@notexpectedyet.com>
2020-07-19 14:31:56 +0100

    up revved

--- routes/index.js
+++ routes/index.js
@@ -1,146 +1,146 @@
-const express = require("express");
+const express = require('express')
 
-const router = express.Router();
-const { ensureAuthenticated } = require("../config/auth");
-const db = require("../config/db").MongoURI;
-const pjson = require("../package.json");
-const ServerSettings = require("../models/ServerSettings.js");
-const prettyHelpers = require("../views/partials/functions/pretty.js");
-const runner = require("../runners/state.js");
+const router = express.Router()
+const { ensureAuthenticated } = require('../config/auth')
+const db = require('../config/db').MongoURI
+const pjson = require('../package.json')
+const ServerSettings = require('../models/ServerSettings.js')
+const prettyHelpers = require('../views/partials/functions/pretty.js')
+const runner = require('../runners/state.js')
 
-const { Runner } = runner;
-const _ = require("lodash");
+const { Runner } = runner
+const _ = require('lodash')
 
-const historyClean = require("../lib/dataFunctions/historyClean.js");
+const historyClean = require('../lib/dataFunctions/historyClean.js')
 
-const { HistoryClean } = historyClean;
-const filamentClean = require("../lib/dataFunctions/filamentClean.js");
+const { HistoryClean } = historyClean
+const filamentClean = require('../lib/dataFunctions/filamentClean.js')
 
-const { FilamentClean } = filamentClean;
-const settingsClean = require("../lib/dataFunctions/settingsClean.js");
+const { FilamentClean } = filamentClean
+const settingsClean = require('../lib/dataFunctions/settingsClean.js')
 
-const { SettingsClean } = settingsClean;
-const printerClean = require("../lib/dataFunctions/printerClean.js");
+const { SettingsClean } = settingsClean
+const printerClean = require('../lib/dataFunctions/printerClean.js')
 
-const { PrinterClean } = printerClean;
-const fileClean = require("../lib/dataFunctions/fileClean.js");
+const { PrinterClean } = printerClean
+const fileClean = require('../lib/dataFunctions/fileClean.js')
 
-const { FileClean } = fileClean;
+const { FileClean } = fileClean
 
-const version = `${pjson.version}.6.2`;
-console.log(`Version: ${version}`);
-console.log(`db: ${db}`);
+const version = `${pjson.version}.6.5`
+console.log(`Version: ${version}`)
+console.log(`db: ${db}`)
 
 // Welcome Page
-async function welcome() {
-  if (db === "") {
+async function welcome () {
+  if (db === '') {
     // No db setup, show db warning before login.
-    router.get("/", (req, res) =>
-      res.render("database", { page: "Database Warning" })
-    );
+    router.get('/', (req, res) =>
+      res.render('database', { page: 'Database Warning' })
+    )
   } else {
-    const serverSettings = await ServerSettings.find({});
+    const serverSettings = await ServerSettings.find({})
     if (serverSettings[0].server.loginRequired === false) {
-      router.get("/", (req, res) => res.redirect("/dashboard"));
+      router.get('/', (req, res) => res.redirect('/dashboard'))
     } else {
-      const { registration } = serverSettings[0].server;
-      router.get("/", (req, res) =>
-        res.render("welcome", {
-          page: "Welcome",
+      const { registration } = serverSettings[0].server
+      router.get('/', (req, res) =>
+        res.render('welcome', {
+          page: 'Welcome',
           registration,
-          serverSettings: serverSettings[0],
+          serverSettings: serverSettings[0]
         })
-      );
+      )
     }
   }
 }
-welcome();
+welcome()
 
 // Dashboard Page
-router.get("/dashboard", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("dashboard", {
+  res.render('dashboard', {
     name: user,
     userGroup: group,
     version,
     printerCount: printers.length,
-    page: "Dashboard",
-    helpers: prettyHelpers,
-  });
-});
-router.get("/printers", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+    page: 'Dashboard',
+    helpers: prettyHelpers
+  })
+})
+router.get('/printers', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("printerManagement", {
+  res.render('printerManagement', {
     name: user,
     userGroup: group,
     version,
-    page: "Printer Manager",
+    page: 'Printer Manager',
     printerCount: printers.length,
-    helpers: prettyHelpers,
-  });
-});
+    helpers: prettyHelpers
+  })
+})
 // File Manager Page
-router.get("/filemanager", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  const currentOperations = await PrinterClean.returnCurrentOperations();
-  const fileStatistics = await FileClean.returnStatistics();
-  let user = null;
-  let group = null;
+router.get('/filemanager', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  const currentOperations = await PrinterClean.returnCurrentOperations()
+  const fileStatistics = await FileClean.returnStatistics()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("filemanager", {
+  res.render('filemanager', {
     name: user,
     userGroup: group,
     version,
-    page: "Printer Manager",
+    page: 'Printer Manager',
     printerCount: printers.length,
     helpers: prettyHelpers,
     currentOperationsCount: currentOperations.count,
-    fileStatistics,
-  });
-});
+    fileStatistics
+  })
+})
 // History Page
-router.get("/history", ensureAuthenticated, async (req, res) => {
-  const printers = Runner.returnFarmPrinters();
-  const history = await HistoryClean.returnHistory();
-  const statistics = await HistoryClean.returnStatistics();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+router.get('/history', ensureAuthenticated, async (req, res) => {
+  const printers = Runner.returnFarmPrinters()
+  const history = await HistoryClean.returnHistory()
+  const statistics = await HistoryClean.returnStatistics()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("history", {
+  res.render('history', {
     name: user,
     userGroup: group,
     version,
@@ -148,166 +148,166 @@ router.get("/history", ensureAuthenticated, async (req, res) => {
     history,
     printStatistics: statistics,
     helpers: prettyHelpers,
-    page: "History",
-  });
-});
+    page: 'History'
+  })
+})
 // Panel view  Page
-router.get("/mon/panel", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const sortedIndex = await Runner.sortedIndex();
-  const clientSettings = await SettingsClean.returnClientSettings();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+router.get('/mon/panel', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const sortedIndex = await Runner.sortedIndex()
+  const clientSettings = await SettingsClean.returnClientSettings()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("panelView", {
+  res.render('panelView', {
     name: user,
     userGroup: group,
     version,
     printers,
     printerCount: printers.length,
     sortedIndex,
-    page: "Panel View",
+    page: 'Panel View',
     helpers: prettyHelpers,
-    clientSettings,
-  });
-});
+    clientSettings
+  })
+})
 // Camera view  Page
-router.get("/mon/camera", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const sortedIndex = await Runner.sortedIndex();
-  const clientSettings = await SettingsClean.returnClientSettings();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+router.get('/mon/camera', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const sortedIndex = await Runner.sortedIndex()
+  const clientSettings = await SettingsClean.returnClientSettings()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("cameraView", {
+  res.render('cameraView', {
     name: user,
     userGroup: group,
     version,
     printers,
     printerCount: printers.length,
     sortedIndex,
-    page: "Panel View",
+    page: 'Camera View',
     helpers: prettyHelpers,
-    clientSettings,
-  });
-});
+    clientSettings
+  })
+})
 // List view  Page
-router.get("/mon/list", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const sortedIndex = await Runner.sortedIndex();
-  const clientSettings = await SettingsClean.returnClientSettings();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+router.get('/mon/list', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const sortedIndex = await Runner.sortedIndex()
+  const clientSettings = await SettingsClean.returnClientSettings()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("listView", {
+  res.render('listView', {
     name: user,
     userGroup: group,
     version,
     printers,
     printerCount: printers.length,
     sortedIndex,
-    page: "Panel View",
+    page: 'List View',
     helpers: prettyHelpers,
-    clientSettings,
-  });
-});
-router.get("/mon/currentOp", ensureAuthenticated, async (req, res) => {
-  const printers = await Runner.returnFarmPrinters();
-  const sortedIndex = await Runner.sortedIndex();
-  const clientSettings = await SettingsClean.returnClientSettings();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  let user = null;
-  let group = null;
+    clientSettings
+  })
+})
+router.get('/mon/currentOp', ensureAuthenticated, async (req, res) => {
+  const printers = await Runner.returnFarmPrinters()
+  const sortedIndex = await Runner.sortedIndex()
+  const clientSettings = await SettingsClean.returnClientSettings()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("currentOperationsView", {
+  res.render('currentOperationsView', {
     name: user,
     userGroup: group,
     version,
     printers,
     printerCount: printers.length,
     sortedIndex,
-    page: "Panel View",
+    page: 'Current Operations',
     helpers: prettyHelpers,
-    clientSettings,
-  });
-});
-router.get("/filament", ensureAuthenticated, async (req, res) => {
-  const printers = Runner.returnFarmPrinters();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  const statistics = await FilamentClean.getStatistics();
-  const spools = await FilamentClean.getSpools();
-  const profiles = await FilamentClean.getProfiles();
-  let user = null;
-  let group = null;
+    clientSettings
+  })
+})
+router.get('/filament', ensureAuthenticated, async (req, res) => {
+  const printers = Runner.returnFarmPrinters()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  const statistics = await FilamentClean.getStatistics()
+  const spools = await FilamentClean.getSpools()
+  const profiles = await FilamentClean.getProfiles()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("filament", {
+  res.render('filament', {
     name: user,
     userGroup: group,
     version,
     printerCount: printers.length,
-    page: "Filament Manager",
+    page: 'Filament Manager',
     helpers: prettyHelpers,
     serverSettings,
     spools,
     profiles,
-    statistics,
-  });
-});
-router.get("/system", ensureAuthenticated, async (req, res) => {
-  const clientSettings = await SettingsClean.returnClientSettings();
-  const serverSettings = await SettingsClean.returnSystemSettings();
-  const printers = Runner.returnFarmPrinters();
-  let user = null;
-  let group = null;
+    statistics
+  })
+})
+router.get('/system', ensureAuthenticated, async (req, res) => {
+  const clientSettings = await SettingsClean.returnClientSettings()
+  const serverSettings = await SettingsClean.returnSystemSettings()
+  const printers = Runner.returnFarmPrinters()
+  let user = null
+  let group = null
   if (serverSettings.server.loginRequired === false) {
-    user = "No User";
-    group = "Administrator";
+    user = 'No User'
+    group = 'Administrator'
   } else {
-    user = req.user.name;
-    group = req.user.group;
+    user = req.user.name
+    group = req.user.group
   }
-  res.render("system", {
+  res.render('system', {
     name: user,
     userGroup: group,
     version,
     printerCount: printers.length,
-    page: "System",
+    page: 'System',
     helpers: prettyHelpers,
     clientSettings,
-    serverSettings,
-  });
-});
-module.exports = router;
+    serverSettings
+  })
+})
+module.exports = router
