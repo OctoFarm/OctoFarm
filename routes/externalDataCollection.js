f7b95244ba01ac1c6b9a8e2b752947c2c8a50967
James Mackay <info@notexpectedyet.com>
2020-08-12 04:53:09 +0100

    remove get

--- routes/externalDataCollection.js
+++ routes/externalDataCollection.js
@@ -4,15 +4,12 @@ const router = express.Router();
 
 const { ensureAuthenticated } = require('../config/auth');
 
+const RoomData = require("../models/roomData.js");
+
 router.post('/roomData', ensureAuthenticated, async (req, res) => {
     const enviromentData = req.body;
-    const RoomData = require("../models/roomData.js");
     const databaseData = await new RoomData(enviromentData);
     databaseData.save();
 });
-router.get('/roomData', ensureAuthenticated, async (req, res) => {
-    const RoomData = require("../models/roomData.js");
-    const roomDataCollection = await RoomData.find({});
-    console.log(roomDataCollection);
-});
+
 module.exports = router;
