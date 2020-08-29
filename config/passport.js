911f62db12a8c54cd18ab41373bb44dc3d78d00f
James Mackay <info@notexpectedyet.com>
2020-08-29 07:50:45 +0100

    linted

--- config/passport.js
+++ config/passport.js
@@ -6,41 +6,41 @@ const RememberMeStrategy = require("passport-remember-me").Strategy;
 const User = require("../models/User.js");
 
 module.exports = function (passport) {
-  passport.use(
-    new LocalStrategy(
-      { usernameField: "username" },
-      (username, password, done) => {
-        // Match User in db
-        User.findOne({ username })
-          .then((user) => {
-            if (!user) {
-              return done(null, false, {
-                message: "That username is not registered",
-              });
+    passport.use(
+        new LocalStrategy(
+            { usernameField: "username" },
+            (username, password, done) => {
+                // Match User in db
+                User.findOne({ username })
+                    .then((user) => {
+                        if (!user) {
+                            return done(null, false, {
+                                message: "That username is not registered",
+                            });
+                        }
+
+                        // Match password
+                        bcrypt.compare(password, user.password, (err, isMatch) => {
+                            if (err) throw err;
+
+                            if (isMatch) {
+                                return done(null, user);
+                            }
+                            return done(null, false, { message: "Password incorrect" });
+                        });
+                    })
+                    .catch((err) => console.log(err));
             }
+        )
+    );
 
-            // Match password
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-              if (err) throw err;
-
-              if (isMatch) {
-                return done(null, user);
-              }
-              return done(null, false, { message: "Password incorrect" });
-            });
-          })
-          .catch((err) => console.log(err));
-      }
-    )
-  );
-
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user);
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
     });
-  });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        });
+    });
 };
