c49d7c7d26fa1b438b672f609f03c49c26048b1f
James Mackay <info@notexpectedyet.com>
2020-09-16 06:32:55 +0100

    add client settings to schema ready for split to per user client settings

--- models/User.js
+++ models/User.js
@@ -1,26 +1,30 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  group: {
-    type: String,
-    required: false
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    group: {
+        type: String,
+        required: false
+    },
+    clientSettings: {
+        type: Object,
+        required: false,
+    }
 });
 
 const User = mongoose.model("User", UserSchema);
