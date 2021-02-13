b4cec5704d9b8fd5e5868a7116b836d9db45a8da
notexpectedyet <online@notexpectedyet.com>
2021-02-13 09:39:24 +0000

    fix my eslint settings
    Signed-off-by: notexpectedyet <online@notexpectedyet.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,29 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: [
-
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: "babel-eslint",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "max-len": [
+      "error",
+      {
+        code: 300,
+        ignoreComments: true,
+        ignoreUrls: true,
+      },
     ],
-
-    parserOptions: {
-        ecmaVersion: 2020,
-        parser: 'babel-eslint',
-        sourceType: "module"
-    },
-    rules: {
-        'max-len': [
-            'error',
-            {
-                code: 300,
-                ignoreComments: true,
-                ignoreUrls: true,
-            },
-        ],
-        "global-require": "off",
-        "quotes": ["error", "double"],
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
+    "global-require": "off",
+    quotes: ["error", "double"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
 };
