c064007d2543136c146adcdfd25ab65febe8fa13
James Mackay <info@notexpectedyet.com>
2021-02-20 06:47:33 +0000

    Cleaned up views
    Removed uneeded views from current client

--- client_web/src/router/index.js
+++ client_web/src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Dashboard from "../views/Dashboard";
+import Dashboard from "../views/Dashboard.vue";
 import PrinterManager from "../views/admin/PrinterManager.vue";
 import Monitoring from "../views/Monitoring.vue";
 import FileManager from "../views/FileManager.vue";
 import History from "../views/History.vue";
-import StockManager from "../views/StockManager.vue";
-import Maintenance from "../views/Maintenance.vue";
+// import StockManager from "../views/StockManager.vue";
+// import Maintenance from "../views/Maintenance.vue";
 import About from "../views/About.vue";
 import Alerts from "../views/admin/Alerts.vue";
 import DatabaseManager from "../views/admin/DatabaseManager.vue";
@@ -22,92 +22,92 @@ const routes = [
     path: "/",
     name: "Dashboard",
     icon: "mdi-view-dashboard-variant",
-    component: Dashboard
+    component: Dashboard,
   },
   {
     path: "/schedule",
     name: "Scheduler",
     icon: "mdi-calendar-text",
-    component: JobQueues
+    component: JobQueues,
   },
   {
     name: "Monitoring",
     icon: "mdi-eye",
     path: "/monitoring",
-    component: Monitoring
+    component: Monitoring,
   },
   {
     name: "File Manager",
     icon: "mdi-file-code",
     path: "/file-manager",
-    component: FileManager
+    component: FileManager,
   },
   {
     name: "History",
     icon: "mdi-history",
     path: "/history",
-    component: History
+    component: History,
   },
-  {
-    name: "Stock Manager",
-    icon: "mdi-warehouse",
-    path: "/stock",
-    component: StockManager
-  },
-  {
-    name: "Maintenance",
-    icon: "mdi-hammer-wrench",
-    path: "/maintenance",
-    component: Maintenance
-  },
+  // {
+  //   name: "Stock Manager",
+  //   icon: "mdi-warehouse",
+  //   path: "/stock",
+  //   component: StockManager,
+  // },
+  // {
+  //   name: "Maintenance",
+  //   icon: "mdi-hammer-wrench",
+  //   path: "/maintenance",
+  //   component: Maintenance,
+  // },
   {
     name: "Printer Manager",
     icon: "mdi-printer-3d",
     path: "/printers",
-    component: PrinterManager
+    component: PrinterManager,
   },
   {
     name: "User Accounts",
     icon: "mdi-account-reactivate",
     path: "/admin/system",
-    component: UserAccounts
+    component: UserAccounts,
   },
   {
     name: "Alerts",
     icon: "mdi-shield-alert",
     path: "/admin/alerts",
-    component: Alerts
+    component: Alerts,
   },
   {
     name: "Server Settings",
     icon: "mdi-cog",
     path: "/admin/server",
-    component: ServerSettings
+    component: ServerSettings,
   },
   {
     name: "Database Manager",
     icon: "mdi-database-edit",
     path: "/admin/database",
-    component: DatabaseManager
+    component: DatabaseManager,
   },
   {
     name: "Logs",
     icon: "mdi-card-text",
     path: "/admin/logs",
-    component: Logs
+    component: Logs,
   },
   {
     name: "About",
     icon: "mdi-information",
     path: "/about",
-    component: About
-  }
+    component: About,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
