6eb1f16fece259eb1030b2cea04e0b5c1810aab5
David Zwart <davidzwa@gmail.com>
2021-02-28 14:10:38 +0100

    Bug

--- server_2x/test/base/test-setup.global.ts
+++ server_2x/test/base/test-setup.global.ts
@@ -1,7 +1,7 @@
 import {start} from "mongo-unit";
 import {JWT_EXPIRES_IN_KEY, JWT_SECRET_KEY} from "../../src/auth/auth.config";
 
-export const e2ePort = 27017;
+export const e2ePort = parseInt(process.env.TEST_DB_PORT,10) || 27017;
 export default async () => {
     console.log('DB warmup');
     process.env[JWT_SECRET_KEY] = "testenvironmentv11.1.1";
