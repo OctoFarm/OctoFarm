120f012a57d91b7b86316e1791e871e74d899b5a
James Mackay <info@notexpectedyet.com>
2021-03-04 14:18:14 +0000

    make systemInfo null, check for such on client to stop page failing if unable to generate

--- server_src/runners/systemInfo.js
+++ server_src/runners/systemInfo.js
@@ -1,7 +1,10 @@
 const si = require("systeminformation");
 const process = require("process");
+const Logger = require("../lib/logger.js");
 
-let systemInfo = {};
+const logger = new Logger("OctoFarm-Server");
+
+let systemInfo = null;
 
 class SystemRunner {
   static returnInfo() {
@@ -20,50 +23,55 @@ class SystemRunner {
   static async getSystemInfo() {
     try {
       //Collect some system information
-      const cpu = await si.cpu().catch((error) => console.error(error));
+      const cpu = await si.cpu().catch((error) => logger.error(error));
 
       const cpuCurrentSpeed = await si
         .cpuCurrentspeed()
-        .catch((error) => console.error(error));
+        .catch((error) => logger.error(error));
       const cpuLoad = await si
         .currentLoad()
-        .catch((error) => console.error(error));
-      const cpuInfo = {
-        cpu,
-        speed: cpuCurrentSpeed,
-      };
+        .catch((error) => logger.error(error));
+      // const cpuInfo = {
+      //   cpu,
+      //   speed: cpuCurrentSpeed,
+      // };
 
-      const memoryInfo = await si.mem().catch((error) => console.error(error));
+      const memoryInfo = await si.mem().catch((error) => logger.error(error));
       const uptime = si.time();
-      const osInfo = await si.osInfo().catch((error) => console.error(error));
+      const osInfo = await si.osInfo().catch((error) => logger.error(error));
       const currentProcess = await si
         .processes()
-        .catch((error) => console.error(error));
+        .catch((error) => logger.error(error));
       let sysProcess = {};
       currentProcess.list.forEach((current) => {
         if (current.pid === process.pid) {
           sysProcess = current;
         }
       });
-      const fileSize = await si.fsSize().catch((error) => console.error(error));
+      const fileSize = await si.fsSize().catch((error) => logger.error(error));
       const systemDisk = fileSize[0];
 
+      //This maybe related to node 13.12.0 possibly. Issue #341.
       let warnings = {};
+
       if (typeof systemDisk !== "undefined") {
         if (systemDisk.use >= 99) {
           warnings = {
             status: "danger",
-            message: `Danger! Your disk is over 99% full... OctoFarms operations could be effected if you don't clean up some space or move to a larger hard drive.`,
+            message:
+              "Danger! Your disk is over 99% full... OctoFarms operations could be effected if you don't clean up some space or move to a larger hard drive.",
           };
         } else if (systemDisk.use >= 95) {
           warnings = {
             status: "warning",
-            message: `Warning your disk is over 95% full... Please clean up some space or move to a larger hard drive.`,
+            message:
+              "Warning your disk is over 95% full... Please clean up some space or move to a larger hard drive.",
           };
         } else if (systemDisk.use >= 90) {
           warnings = {
             status: "warning",
-            message: `Warning your disk is getting full... Please clean up some space or move to a larger hard drive.`,
+            message:
+              "Warning your disk is getting full... Please clean up some space or move to a larger hard drive.",
           };
         }
       }
@@ -81,7 +89,11 @@ class SystemRunner {
       };
       return systemInfo;
     } catch (e) {
-      console.log(e);
+      logger.error(
+        "Some system information has failed to generate:",
+        e.message
+      );
+
       return false;
     }
   }
