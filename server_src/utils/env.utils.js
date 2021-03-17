7686f19ef000df0122ff15a9a8741cdfc558c8f7
David Zwart <davidzwa@gmail.com>
2021-03-17 17:13:11 +0100

    Ignore background image and copy a default if not present as to keep it untracked

--- server_src/utils/env.utils.js
+++ server_src/utils/env.utils.js
@@ -1,5 +1,6 @@
 const {execSync} = require('child_process');
 const path = require("path");
+const fs = require("fs");
 
 function isPm2() {
   return 'PM2_HOME' in process.env || 'PM2_JSON_PROCESSING' in process.env || 'PM2_CLI' in process.env;
@@ -13,15 +14,15 @@ function isNode() {
   return 'NODE' in process.env;
 }
 
-function verifyPackageJsonRequirements(directoryPath) {
-  const dirConts = require("fs").readdirSync(directoryPath);
+function verifyPackageJsonRequirements(rootPath) {
+  const dirConts = fs.readdirSync(rootPath);
   const hasPackageJson = dirConts.includes("package.json");
   if (!hasPackageJson) {
-    console.error(`FAILURE. Could not find 'package.json' in root folder ${directoryPath}`);
+    console.error(`FAILURE. Could not find 'package.json' in root folder ${rootPath}`);
     return false;
   } else {
     console.info("✓ found 'package.json'");
-    const packageName = require(path.join(directoryPath, "package.json")).name;
+    const packageName = require(path.join(rootPath, "package.json")).name;
     if (!packageName) {
       console.error("X Could not find 'name' property in package.json file. Aborting OctoFarm.");
       return false;
@@ -34,9 +35,23 @@ function verifyPackageJsonRequirements(directoryPath) {
   return true;
 }
 
+function ensureBackgroundImageExists(rootPath) {
+  const targetBgDir = path.join(rootPath, "images")
+  const targetBgPath = path.join(targetBgDir, "bg.jpg");
+  if (!fs.existsSync(targetBgDir)) {
+    fs.mkdirSync(targetBgDir);
+  }
+  const bgFileExists = fs.existsSync(targetBgPath);
+  if (!bgFileExists) {
+    fs.copyFileSync(path.join(__dirname, "bg_default.jpg"), targetBgPath);
+    console.log(`✓ Copyied default background image to ${targetBgPath} as it was not found.`);
+  }
+}
+
 module.exports = {
   isPm2,
   isNodemon,
   isNode,
-  verifyPackageJsonRequirements
+  verifyPackageJsonRequirements,
+  ensureBackgroundImageExists
 }
