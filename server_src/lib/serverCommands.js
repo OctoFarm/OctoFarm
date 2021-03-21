b42dfcdb2fc48af73143e145be9b5b8e633839be
James Mackay <info@notexpectedyet.com>
2021-03-21 13:16:17 +0000

    Clean up server commands
    Split Logger for Scripts and Server, Throw error check before stdout is checked, update message for successful update to user.

--- server_src/lib/serverCommands.js
+++ server_src/lib/serverCommands.js
@@ -3,7 +3,8 @@ const path = require("path");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const Logger = require("../lib/logger.js");
-const logger = new Logger("OctoFarm-Scripts");
+const loggerScripts = new Logger("OctoFarm-Scripts");
+const loggerServer = new Logger("OctoFarm-Server");
 const wol = require("wake_on_lan");
 
 function isGitSync(dir) {
@@ -12,7 +13,7 @@ function isGitSync(dir) {
 
 class SystemCommands {
   static async rebootOctoFarm() {
-    logger.info("Restart OctoFarm server requests");
+    loggerServer.info("Restart OctoFarm server requests");
     let checkForNamedService = false;
     // Bit hacky... Make sure OctoFarm has a pid under pm2.... Had to stagger the actual restart command so the return would send to client. - Open to suggestions
     try {
@@ -21,18 +22,17 @@ class SystemCommands {
       if (isNaN(parseInt(stdout))) {
         throw "No process number returned";
       }
-      logger.info("stdout:", stdout);
       checkForNamedService = true;
       // If the process had a integer pid succeeded then we are ok to restart, errors are caught by the try, catch
       exec("pm2 restart OctoFarm");
     } catch (err) {
-      logger.error(err);
+      loggerServer.error(err);
     }
     return checkForNamedService;
   }
   // This will need changing when .deb / installation script becomes a thing. It's built to deal with the current implementation.
   static async updateOctoFarm() {
-    logger.info("Update OctoFarm server requests");
+    loggerServer.info("Update OctoFarm server requests");
     let haveWeSuccessfullyUpdatedOctoFarm = false;
     let statusTypeForUser = null;
     let messageForUser = null;
@@ -45,23 +45,31 @@ class SystemCommands {
         statusTypeForUser = "error";
         throw notGitRepoError;
       }
-      logger.info("Root location contain a git folder: ", doWeHaveAGitFolder);
 
       // We are inside a folder which was created with git... okay to continue the pull
       const { stdout, stderr } = await exec("git pull");
+      // Throw on any error, nobody should be editing their local github folder after this release is made. If they do, they need to manually update.
+      if (stderr) {
+        let manualChangesError = "Manual changes to local OctoFarm folder detected, manual update required...";
+        messageForUser = manualChangesError;
+        statusTypeForUser = "error";
+        throw stderr;
+      }
+      // Catch if user somehow circumvents the client button checks in place.
       if (stdout.includes("Already up-to-date")) {
         let alreadyUpToDateError =
           "Already up to date! Ignoring update request...";
         statusTypeForUser = "warning";
         messageForUser = alreadyUpToDateError;
         throw alreadyUpToDateError;
-      }
-      if (stderr) {
-        throw "No process number returned";
+      } else {
+        statusTypeForUser = "success";
+        messageForUser =
+          "Update command has run successfully, you need to restart the OctoFarm service to activate these changes...";
       }
       haveWeSuccessfullyUpdatedOctoFarm = true;
     } catch (err) {
-      logger.error(err);
+      loggerServer.error("OctoFarm Update Error: ", err);
     }
     return {
       haveWeSuccessfullyUpdatedOctoFarm,
@@ -73,15 +81,15 @@ class SystemCommands {
 
 class Script {
   static async fire(scriptLocation, message) {
-    logger.info("Script: ", scriptLocation);
-    logger.info("Message: ", message);
+    loggerScripts.info("Script: ", scriptLocation);
+    loggerScripts.info("Message: ", message);
     try {
       const { stdout, stderr } = await exec(`${scriptLocation} ${message}`);
-      logger.info("stdout:", stdout);
-      logger.info("stderr:", stderr);
+      loggerScripts.info("stdout:", stdout);
+      loggerScripts.info("stderr:", stderr);
       return scriptLocation + ": " + stdout;
     } catch (err) {
-      logger.error(err);
+      loggerScripts.error(err);
       return err;
     }
   }
@@ -97,9 +105,9 @@ class Script {
 
     wol.wake(mac, function (error) {
       if (error) {
-        logger.error("Couldn't fire wake packet", error);
+        loggerScripts.error("Couldn't fire wake packet", error);
       } else {
-        logger.info("Successfully fired wake packet: ", mac, opts);
+        loggerScripts.info("Successfully fired wake packet: ", mac, opts);
       }
     });
   }
