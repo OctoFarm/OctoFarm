eea89ac342f91657323b7afd476b36ff595fd09e
David Zwart <davidzwa@gmail.com>
2021-04-04 22:23:06 +0200

    Fix le tests

--- server_2x/src/octoprint/state/client-connection.state.spec.ts
+++ server_2x/src/octoprint/state/client-connection.state.spec.ts
@@ -4,6 +4,7 @@ import {OctoPrintClientService} from "../services/octoprint-client.service";
 import {HttpService} from "@nestjs/common";
 import {of} from "rxjs";
 import {RestConnectionParams} from "../models/rest-connection.params";
+import {WebsocketClientService} from "../services/websocket-client.service";
 
 describe(ClientConnectionsState.name, () => {
     let service: ClientConnectionsState;
@@ -14,6 +15,7 @@ describe(ClientConnectionsState.name, () => {
             providers: [
                 ClientConnectionsState,
                 OctoPrintClientService,
+                WebsocketClientService,
                 {
                     provide: HttpService,
                     useValue: {
@@ -33,7 +35,7 @@ describe(ClientConnectionsState.name, () => {
     });
 
     it('can call init with defaulting state resulting', async () => {
-        await service.initState(testParams);
+        await service.initState(testParams, 'ws');
         const clientConnectionState = service.getState();
         expect(clientConnectionState).toBeTruthy();
         expect(clientConnectionState.websocketConnected).toEqual(undefined);
@@ -41,18 +43,18 @@ describe(ClientConnectionsState.name, () => {
     });
 
     it('cannot call init again without error', async () => {
-        await service.initState(testParams);
-        await expect(service.initState(testParams)).rejects.toThrow("Can't initialize already known state.");
+        await service.initState(testParams, 'ws');
+        await expect(service.initState(testParams, 'ws')).rejects.toThrow("Can't initialize already known state.");
     });
 
     it('improper apiKey will show up as invalid state', async () => {
         const illegalTestParams = new RestConnectionParams("http://notexistedyet", "WAYTOOSHORT");
-        await service.initState(illegalTestParams);
+        await service.initState(illegalTestParams, 'ws');
         expect(service.getState().apiKeyValid).toEqual(false);
     });
 
     it('should get frozen state', async () => {
-        await service.initState(testParams);
+        await service.initState(testParams, 'ws');
         const frozenState = service.getState();
         expect(Object.isFrozen(frozenState)).toBe(true);
     });
