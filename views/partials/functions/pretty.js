7a7ef609baf29dd81017d7a94d68f6da3cf1a57d
James Mackay <info@notexpectedyet.com>
2021-04-24 15:53:46 +0100

    Fixed #438 - Moved toFixed() function inside the generateBytes functi… (#446)
    * Fixed #438 - Moved toFixed() function inside the generateBytes function in pretty helpers. Stops the page failing when trying toFixed a string.
    
    * changelog
    
    * Fixed #438 - Moved toFixed() function inside the generateBytes function in pretty helpers. Stops the page failing when trying toFixed a string.
    
    * changelog
    
    * clean up of bytes function

--- views/partials/functions/pretty.js
+++ views/partials/functions/pretty.js
@@ -10,7 +10,7 @@ const bytes = function (a, b) {
       d = b || 2,
       e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
       f = Math.floor(Math.log(a) / Math.log(c));
-    return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f];
+    return parseFloat(a / Math.pow(c, f)).toFixed(d) + " " + e[f];
   }
 };
 
