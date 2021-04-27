5971d2918790f9e90c6d041fc3c2a946c540a791
NotExpectedYet <online@notexpectedyet.com>
2021-04-27 07:13:56 +0100

    added ensurePageTitle function

--- app-env.js
+++ app-env.js
@@ -205,6 +205,7 @@ function setupEnvConfig(skipDotEnv = false) {
   ensureMongoDBConnectionStringSet();
   ensurePortSet();
   envUtils.ensureBackgroundImageExists(__dirname);
+  ensurePageTitle();
 }
 
 function getViewsPath() {
@@ -232,6 +233,18 @@ function getViewsPath() {
     logger.info("✓ Views folder found:", viewsPath);
   }
   return viewsPath;
+}
+
+function ensurePageTitle() {
+  if (!process.env[AppConstants.OCTOFARM_SITE_TITLE_KEY]) {
+    logger.info(
+        `~ ${AppConstants.OCTOFARM_SITE_TITLE_KEY} environment variable is not set. Assuming default: ${AppConstants.OCTOFARM_SITE_TITLE_KEY}=${AppConstants.defaultOctoFarmPageTitle}.`
+    );
+    printInstructionsURL();
+    process.env[
+        AppConstants.OCTOFARM_SITE_TITLE_KEY
+        ] = AppConstants.defaultOctoFarmPageTitle.toString();
+  }
 }
 
 module.exports = {
