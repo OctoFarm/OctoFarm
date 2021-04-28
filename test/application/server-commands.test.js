d483cbded22bad17664f9efc2c206fbec2fb6ae8
David Zwart <davidzwa@gmail.com>
2021-04-28 12:06:00 +0200

    Commit for safety

---
+++ test/application/server-commands.test.js
@@ -0,0 +1,8 @@
+describe('ServerCommands', () =>{
+    it("should be able to see non-pushed commits", async () => {
+    });
+
+
+    it("should be able to see uninstalled packages", async () => {
+    });
+});
