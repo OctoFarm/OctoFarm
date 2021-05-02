6cb79af05a01ec3c63481ee5a5c2cd4e3e07a1c7
NotExpectedYet <online@notexpectedyet.com>
2021-05-02 08:03:42 +0100

    npm utilities improvement

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ All notable changes to this project will be documented in this file.
 
 ### Changed
     - Completely re-worked the updater mechanism.
+    - Completely re-worked the npm installation mechanism
 
 ### Removed
 
--- server_src/utils/npm.utils.js
+++ server_src/utils/npm.utils.js
@@ -1,37 +1,39 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-async function doWeHaveMissingPackages() {
-  const { stdout, stderr } = await exec("npm outdated --production");
+async function returnListOfMissingPackages() {
+  try {
+    const outdatedPackageJsonList = await exec(
+      "npm outdated --production --json"
+    );
+    const outdatedPackageListParsed = JSON.parse(
+      outdatedPackageJsonList.stdout
+    );
+    if (outdatedPackageListParsed) {
+      const keys = Object.keys(outdatedPackageListParsed);
+      const missingPackageList = [];
 
-  if (stderr) {
-    throw `Could not reliably check for outdated npm packages, is npm installed? | ${stderr}`;
-  }
-
-  if (stdout) {
-    const matchMissingPackages = new RegExp("((.*?)MISSING.*)", "g");
-    const missingPackages = stdout.match(matchMissingPackages);
-    if (!!missingPackages) {
-      return missingPackages.length > 0;
-    } else {
-      return false;
+      keys.forEach((key, index) => {
+        if (!outdatedPackageListParsed[key].current) {
+          missingPackageList.push(key);
+        }
+      });
+      return missingPackageList;
     }
+  } catch (e) {
+    throw `Error running npm outdated command | ${e}`;
   }
 }
 
-async function installMissingNpmDependencies() {
-  const { stdout, stderr } = await exec("npm install --production");
-
-  if (stderr) {
-    throw `Could not execute npm install, is npm installed? | ${stderr}`;
-  }
-
-  if (stdout) {
-    return true;
+async function installNpmDependencies(dependency) {
+  try {
+    await exec(`npm install ${dependency}`);
+  } catch (e) {
+    throw `Error running installation command | ${e}`;
   }
 }
 
 module.exports = {
-  doWeHaveMissingPackages,
-  installMissingNpmDependencies,
+  returnListOfMissingPackages,
+  installNpmDependencies,
 };
