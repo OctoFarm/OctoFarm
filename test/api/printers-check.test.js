3b658f93e39f90cc8cdba729f324e8f7048f6179
NotExpectedYet <online@notexpectedyet.com>
2021-05-16 06:57:18 +0100

    jest test for 400 and 500 errors returned from update settings endpoint

--- test/api/printers-check.test.js
+++ test/api/printers-check.test.js
@@ -3,7 +3,12 @@ const request = require("supertest");
 const { setupTestApp, getServer } = require("../../app-test");
 
 jest.mock("../../server_src/config/auth");
+
 let app = null;
+const requestBody = {
+  i: "a0a569d20dd308890a1c06",
+};
+
 /**
  * Connect to a new in-memory database before running any tests.
  */
@@ -36,7 +41,7 @@ async function getOrCreateApp() {
   return app;
 }
 
-describe("Printer Update Endpoint", () => {
+describe("Printer Settings Update Endpoint", () => {
   it("should return 400 error when wrong input is provided", async function (done) {
     app = await getOrCreateApp();
     const res = await request(app)
@@ -45,6 +50,24 @@ describe("Printer Update Endpoint", () => {
 
     // Assert input validation failed
     expect(res.statusCode).toEqual(400);
+    expect(res.body).toEqual({});
+    expect(res.res.statusMessage).toEqual("No ID key was provided");
+
+    done();
+  }, 10000);
+
+  it("should return 500 if server fails to update printer settings", async function (done) {
+    app = await getOrCreateApp();
+    const res = await request(app)
+      .post("/printers/updatePrinterSettings")
+      .send(requestBody);
+
+    // Assert server failed
+    expect(res.statusCode).toEqual(500);
+    expect(res.body).toEqual({});
+    expect(res.res.statusMessage).toContain(
+      "The server couldn't update your printer settings!"
+    );
 
     done();
   }, 10000);
