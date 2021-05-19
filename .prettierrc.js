9e4506045b97d8c70104bec6a4fd09f7c86a9b93
David Zwart <davidzwa@gmail.com>
2021-05-19 12:56:07 +0200

    Changed prettier and add + run command

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  trailingComma: "es6",
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
+  trailingComma: "none",
+  tabWidth: 2
 };
