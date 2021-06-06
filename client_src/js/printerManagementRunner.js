1ed5405d0eca046c84c30590f1bb1c20a83bc1cf
NotExpectedYet <online@notexpectedyet.com>
2021-06-06 07:30:47 +0100

    comment code cleanup

--- client_src/js/printerManagementRunner.js
+++ client_src/js/printerManagementRunner.js
@@ -1960,7 +1960,6 @@ const pluginAction = async function (action) {
       });
     }
   }
-
 };
 const blkPluginsBtn = document.getElementById("blkPluginsInstallBtn");
 blkPluginsBtn.addEventListener("click", async (e) => {
