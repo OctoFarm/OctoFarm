7e9c7d31c50bc7f8c7227b9d7a3e2a751057d007
NotExpectedYet <online@notexpectedyet.com>
2021-06-19 15:28:36 +0100

    fix server update test

--- test/api/server-update.test.js
+++ test/api/server-update.test.js
@@ -17,7 +17,7 @@ describe("ServerUpdate Endpoint", () => {
     process.env.testlatest_package_version =
       require("../../package.json").version;
 
-    const res = await request.post("/system/server/update").send();
+    const res = await request.post("/system/update").send();
     expect(res.statusCode).toEqual(302);
     done();
   }, 10000);
