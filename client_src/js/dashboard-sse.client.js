355a9eae76ba6b5fd3c6c21be4445b661eab723a
NotExpectedYet <online@notexpectedyet.com>
2021-06-27 13:33:57 +0100

    move client over to flatted package

--- client_src/js/dashboard-sse.client.js
+++ client_src/js/dashboard-sse.client.js
@@ -1,3 +1,5 @@
+const { parse } = require("flatted");
+
 async function asyncParse(str) {
   try {
     const info = parse(str);
@@ -21,49 +23,3 @@ source.onerror = function () {
 source.onclose = function () {
   postMessage(false);
 };
-
-const Flatted = (function (Primitive, primitive) {
-  const Flatted = {
-    parse: function parse(text, reviver) {
-      const input = JSON.parse(text, Primitives).map(primitives);
-      const value = input[0];
-      const $ = reviver || noop;
-      const tmp =
-        typeof value === "object" && value
-          ? revive(input, new Set(), value, $)
-          : value;
-      return $.call({ "": tmp }, "", tmp);
-    }
-  };
-
-  return Flatted;
-
-  function noop(key, value) {
-    return value;
-  }
-
-  function revive(input, parsed, output, $) {
-    return Object.keys(output).reduce(function (output, key) {
-      const value = output[key];
-      if (value instanceof Primitive) {
-        const tmp = input[value];
-        if (typeof tmp === "object" && !parsed.has(tmp)) {
-          parsed.add(tmp);
-          output[key] = $.call(output, key, revive(input, parsed, tmp, $));
-        } else {
-          output[key] = $.call(output, key, tmp);
-        }
-      } else output[key] = $.call(output, key, value);
-      return output;
-    }, output);
-  }
-
-  function primitives(value) {
-    return value instanceof Primitive ? Primitive(value) : value;
-  }
-
-  function Primitives(key, value) {
-    return typeof value === primitive ? new Primitive(value) : value;
-  }
-})(String, "string");
-var parse = Flatted.parse;
