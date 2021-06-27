cdf15284d96b4c98dad0a702832bfe20239fb76a
NotExpectedYet <online@notexpectedyet.com>
2021-06-27 09:25:51 +0100

    merge missing require for Noty

--- client_src/js/lib/functions/ui.js
+++ client_src/js/lib/functions/ui.js
@@ -1,3 +1,5 @@
+import Noty from "noty";
+
 export default class UI {
   //Colour function
   static getColour(state) {
@@ -46,9 +48,9 @@ export default class UI {
     } else {
       click = [];
     }
-    //This needs a more elegant solution, I think noty is keeping the elements I remove with remove() from the DOM in memory somewhere...
-    noty.setMaxVisible(50);
-    let alert = new noty({
+    //This needs a more elegant solution, I think Noty is keeping the elements I remove with remove() from the DOM in memory somewhere...
+    Noty.setMaxVisible(50);
+    let alert = new Noty({
       type: type,
       theme: "bootstrap-v4",
       closeWith: click,
