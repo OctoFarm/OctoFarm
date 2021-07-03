2e4f546257872c1e3d62fe18230181911685c8a0
James Mackay <info@notexpectedyet.com>
2021-07-03 20:06:45 +0100

    Bugfix/688-670 api fix (#671)
    * default back to true for timeout/recursive
    
    * fix my blind truing
    
    * return back to default false
    
    * fix goofs and clean up user messages
    
    * fix api service mock calling logger when not imported
    
    * revert files to not retry and update api call to use boolean for recursive
    
    * keep a damn changelog
    
    * Revert PiSupport

--- server_src/routes/printers.js
+++ server_src/routes/printers.js
@@ -73,7 +73,7 @@ router.post("/resyncFile", ensureAuthenticated, async (req, res) => {
   if (typeof file.fullPath !== "undefined") {
     ret = await Runner.reSyncFile(file.i, file.fullPath);
   } else {
-    ret = await Runner.getFiles(file.i, "files?recursive=true");
+    ret = await Runner.getFiles(file.i, true);
   }
   setTimeout(function () {
     res.send(ret);
