537c4f14dbc91f7fcb1c7e05c3e33159ffbfdc0b
David Zwart <davidzwa@gmail.com>
2021-08-24 22:43:12 +0200

    Import  typo, changelog

--- server_src/services/octoprint/octoprint-api.service.js
+++ server_src/services/octoprint/octoprint-api.service.js
@@ -8,7 +8,7 @@ const {
 const { checkPluginManagerAPIDeprecation } = require("../../utils/compatibility.utils");
 const Logger = require("../../handlers/logger.js");
 const { processResponse, validatePrinter, constructHeaders } = require("./utils/api.utils");
-const { jsonContentType } = require("../constants/octoprint-service.constants");
+const { jsonContentType } = require("./constants/octoprint-service.constants");
 const { getDefaultTimeout } = require("../../constants/server-settings.constants");
 
 const defaultResponseOptions = { unwrap: true };
