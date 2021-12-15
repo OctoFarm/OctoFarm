c184e0cae99498657daff0139278c6ef661bbf3a
James <james@vexica.com>
2021-12-15 10:15:32 +0000

    fix ui bug

--- client_src/js/lib/modules/temperatureCheck.js
+++ client_src/js/lib/modules/temperatureCheck.js
@@ -10,12 +10,12 @@ export function checkTemps(element, actual, target, tempTriggers, state) {
   if (state === "Complete") {
     if (actual > parseFloat(tempTriggers.coolDown)) {
       const string = `<i class="far fa-circle toolUnder"></i> ${actual}°C <i class="fas fa-bullseye"></i> ${target}°C`;
-      if (element.innerHTML !== string) {
+      if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     } else {
       const string = `<i class="far fa-circle toolUnder"></i> ${actual}°C <i class="fas fa-bullseye toolUnder"></i> ${target}°C`;
-      if (element.innerHTML !== string) {
+      if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     }
@@ -25,23 +25,23 @@ export function checkTemps(element, actual, target, tempTriggers, state) {
       actual < target + parseFloat(tempTriggers.heatingVariation)
     ) {
       const string = `<i class="far fa-circle toolOn"></i> ${actual}°C <i class="fas fa-bullseye toolOn"></i> ${target}°C`;
-      if (element.innerHTML !== string) {
+      if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     } else if (actual < parseFloat(tempTriggers.heatingVariation)) {
       const string = `<i class="far fa-circle"></i> ${actual}°C <i class="fas fa-bullseye"></i> ${target}°C`;
-      if (element.innerHTML !== string) {
+      if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     } else {
       const string = `<i class="far fa-circle toolOut"></i> ${actual}°C <i class="fas fa-bullseye toolOut"></i> ${target}°C`;
-      if (element.innerHTML !== string) {
+      if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     }
   } else {
     const string = `<i class="far fa-circle"></i> ${actual}°C <i class="fas fa-bullseye"></i> ${target}°C`;
-    if (element.innerHTML !== string) {
+    if (element && element.innerHTML !== string) {
       element.innerHTML = string;
     }
   }
