2264ad9a1eae226619261cef8da4ab4eb151383c
James <james@vexica.com>
2021-12-16 08:45:31 +0000

    clean up settings was all over the place

---
+++ server_src/constants/server-issues.constants.js
@@ -0,0 +1,17 @@
+const SERVER_ACTIONS = {
+  ABORTING_BOOT: " Aborting boot... please check logs!"
+};
+module.exports = {
+  SERVER_ISSUES: {
+    DATABASE_AUTH_FAIL: "Database authentication failed..." + SERVER_ACTIONS.ABORTING_BOOT,
+    DATABASE_CONN_FAIL: "Database connection failed..." + SERVER_ACTIONS.ABORTING_BOOT,
+    SERVER_SETTINGS_FAIL_INIT:
+      "Unable to create defaults for server settings..." + SERVER_ACTIONS.ABORTING_BOOT,
+    SERVER_SETTINGS_FAIL_UPDATE:
+      "Unable to update existing server settings..." + SERVER_ACTIONS.ABORTING_BOOT,
+    CLIENT_SETTINGS_FAIL_INIT:
+      "Unable to update existing server settings..." + SERVER_ACTIONS.ABORTING_BOOT,
+    CLIENT_SETTINGS_FAIL_UPDATE:
+      "Unable to update existing server settings..." + SERVER_ACTIONS.ABORTING_BOOT
+  }
+};
