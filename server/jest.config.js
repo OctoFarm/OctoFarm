0ffabd79ca46067e8f1fa6ca79559f00f9291fcf
James Mackay <online@notexpectedyet.com>
2022-03-20 15:19:23 +0000

    clean up and update packages

--- server/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testTimeout: 5000,
-  modulePathIgnorePatterns: ["server_2x"],
-  globalSetup: "./test/setup-global.js",
-  setupFilesAfterEnv: ["./test/setup-after-env.js"]
-};
