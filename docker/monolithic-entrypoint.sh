c91bc29e598a8bb3a7922d0da421d33e7c712fcc
James <james@vexica.com>
2022-04-21 11:06:49 +0100

    hmm docker you mysterious beast

--- docker/monolithic-entrypoint.sh
+++ docker/monolithic-entrypoint.sh
@@ -2,12 +2,9 @@
 mkdir -p /data/db
 nohup sh -c mongod --dbpath /data/db &
 
-pwd
-ENV MONGO=mongodb://127.0.0.1:27017/octofarm
-
-if [ -d "../logs" ]
+if [ -d "logs" ]
 then
-    mkdir -p ../logs
+    mkdir -p logs
 else
     echo "Logs folder already exists..."
 fi
