e7a30609c57a950c739a4a2e2b230eb2cefb99c8
notexpectedyet <online@notexpectedyet.com>
2022-04-23 12:54:49 +0100

    fix temp check spacing

--- client/js/utils/temperature-check.util.js
+++ client/js/utils/temperature-check.util.js
@@ -9,12 +9,12 @@ export function checkTemps(element, actual, target, tempTriggers, state) {
   }
   if (state === "Complete") {
     if (actual > parseFloat(tempTriggers.coolDown)) {
-      const string = `<i class="far fa-circle toolUnder"></i> ${actual}°C <i class="fas fa-bullseye"></i> ${target}°C`;
+      const string = `<i class="far fa-circle toolUnder"></i>&nbsp;${actual}°C&nbsp;<i class="fas fa-bullseye"></i>&nbsp;${target}°C`;
       if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     } else {
-      const string = `<i class="far fa-circle toolUnder"></i> ${actual}°C <i class="fas fa-bullseye toolUnder"></i> ${target}°C`;
+      const string = `<i class="far fa-circle toolUnder"></i>&nbsp;${actual}°C&nbsp;<i class="fas fa-bullseye toolUnder"></i>&nbsp;${target}°C`;
       if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
@@ -24,23 +24,23 @@ export function checkTemps(element, actual, target, tempTriggers, state) {
       actual > target - parseFloat(tempTriggers.heatingVariation) &&
       actual < target + parseFloat(tempTriggers.heatingVariation)
     ) {
-      const string = `<i class="far fa-circle toolOn"></i> ${actual}°C <i class="fas fa-bullseye toolOn"></i> ${target}°C`;
+      const string = `<i class="far fa-circle toolOn"></i>&nbsp;${actual}°C&nbsp;<i class="fas fa-bullseye toolOn"></i>&nbsp;${target}°C`;
       if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     } else if (actual < parseFloat(tempTriggers.heatingVariation)) {
-      const string = `<i class="far fa-circle"></i> ${actual}°C <i class="fas fa-bullseye"></i> ${target}°C`;
+      const string = `<i class="far fa-circle"></i>&nbsp;${actual}°C&nbsp;<i class="fas fa-bullseye"></i>&nbsp;${target}°C`;
       if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     } else {
-      const string = `<i class="far fa-circle toolOut"></i> ${actual}°C <i class="fas fa-bullseye toolOut"></i> ${target}°C`;
+      const string = `<i class="far fa-circle toolOut"></i>&nbsp;${actual}°C&nbsp;<i class="fas fa-bullseye toolOut"></i>&nbsp;${target}°C`;
       if (element && element.innerHTML !== string) {
         element.innerHTML = string;
       }
     }
   } else {
-    const string = `<i class="far fa-circle"></i> ${actual}°C <i class="fas fa-bullseye"></i> ${target}°C`;
+    const string = `<i class="far fa-circle"></i>&nbsp;${actual}°C&nbsp;<i class="fas fa-bullseye"></i>&nbsp;${target}°C`;
     if (element && element.innerHTML !== string) {
       element.innerHTML = string;
     }
