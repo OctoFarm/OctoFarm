e1e87d98c9994cfa4de410b5c6c1beef10a7470d
notexpectedyet <online@notexpectedyet.com>
2022-04-27 02:09:24 +0100

    make power settings onboarding grab only

--- server/services/printers/create-octoprint.service.js
+++ server/services/printers/create-octoprint.service.js
@@ -1176,15 +1176,11 @@ class OctoPrintPrinter {
       this.settingsSystem = system;
       this.settingsWebcam = webcam;
 
-      if (force) {
-        this.powerSettings = testAndCollectPSUControlPlugin(this.powerSettings, plugins);
-      }
-
       //These should not run ever again if this endpoint is forcibly updated. They are for initial scan only.
       if (!force) {
-        this.camURL = acquireWebCamData(this.camURL, this.printerURL, webcam.streamUrl);
-        this.costSettings = testAndCollectCostPlugin(this.costSettings, plugins);
-        this.powerSettings = testAndCollectPSUControlPlugin(this.powerSettings, plugins);
+        this.camURL = acquireWebCamData(this._id, this.camURL, this.printerURL, webcam.streamUrl);
+        this.costSettings = testAndCollectCostPlugin(this._id, this.costSettings, plugins);
+        this.powerSettings = testAndCollectPSUControlPlugin(this._id, this.powerSettings, plugins);
         if (this.settingsAppearance.name === "Grabbing from OctoPrint...") {
           this.settingsAppearance.name = PrinterClean.grabOctoPrintName(
             appearance,
