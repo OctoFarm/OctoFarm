aaffb6cd5d7a5e3d8d43a0c44014976b56c73398
James Mackay <info@notexpectedyet.com>
2022-05-21 07:26:39 +0100

    release(main): release 1.2.1 (#967)
    * chore(main): initial v1.2.1 prep
    
    * chore(main): initial v1.2.1 prep
    
    * fix(server): added an amended timeout time to certain octoprint endpoints that are reportedly slow (#968)
    
    * chore(main): clean up downloads on releases in readme
    
    * chore(main): add an increased timeout to slow timeout calls fixes #963
    
    * chore(docker): update monolithic mongo version to 4.4.14 (#969)
    
    * fix(docker): fix docker containers not starting (#971)
    
    * fix(docker): fix docker containers not starting
    
    * chore(main): update changelog
    
    * chore(docker): fix build scripts
    
    * chore(docker): fix omit dev
    
    * chore(docker): return to omit dev
    
    * chore(docker): try older docker
    
    * chore(docker): lock to mongoose version
    
    * chore(docker): lock to mongoose version try version 10
    
    * chore(docker): grr none work with the old mongo
    
    * chore(docker): revert to mongo 4.4
    
    * chore(docker): fix incorrect version
    
    * chore(docker): try 4.0
    
    * chore(docker): try 3.8
    
    * chore(docker): try 4.0.12
    
    * chore(docker): return docker to latest 4.4
    
    * fix(client): fixed #962
    
    * chore(main): fix git repo for docker
    
    * chore(main): 1.2.1 prep
    
    * chore(server): remove release notes page

--- server/utils/git.utils.js
+++ server/utils/git.utils.js
@@ -5,34 +5,61 @@ const path = resolve(__dirname, "../../");
 const git = simpleGit(path);
 
 function getCurrentBranch() {
+  if (!checkIfWereInAGitRepo()) {
+    return "Not a repository...";
+  }
   return git.branch();
 }
 
 function makeSureBranchIsUpToDateWithRemote() {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return git.fetch();
 }
 
 function checkIfWereInAGitRepo() {
-  return git.checkIsRepo();
+  let response;
+  try {
+    response = git.checkIsRepo();
+  } catch (e) {
+    console.warn("Not a git repository, ignoring...");
+  }
+  return response;
 }
 
 function returnCurrentGitStatus() {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return git.status();
 }
 
 function isBranchUpToDate(status) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return status.behind === 0;
 }
 
 function isBranchInfront(status) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return status.ahead !== 0;
 }
 
 function getListOfModifiedFiles(status) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return status.modified;
 }
 
 async function pullLatestRepository(force) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   if (force) {
     await git.reset("hard");
     return git.pull();
