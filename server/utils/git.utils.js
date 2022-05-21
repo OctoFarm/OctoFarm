792223c660d8c7884a7ffdc43e2daa0d451b113d
notexpectedyet <online@notexpectedyet.com>
2022-05-21 07:13:44 +0100

    chore(main): fix git repo for docker

--- server/utils/git.utils.js
+++ server/utils/git.utils.js
@@ -5,10 +5,16 @@ const path = resolve(__dirname, "../../");
 const git = simpleGit(path);
 
 function getCurrentBranch() {
+  if (!checkIfWereInAGitRepo()) {
+    return "Not a repository...";
+  }
   return git.branch();
 }
 
 function makeSureBranchIsUpToDateWithRemote() {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return git.fetch();
 }
 
@@ -23,22 +29,37 @@ function checkIfWereInAGitRepo() {
 }
 
 function returnCurrentGitStatus() {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return git.status();
 }
 
 function isBranchUpToDate(status) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return status.behind === 0;
 }
 
 function isBranchInfront(status) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return status.ahead !== 0;
 }
 
 function getListOfModifiedFiles(status) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   return status.modified;
 }
 
 async function pullLatestRepository(force) {
+  if (!checkIfWereInAGitRepo()) {
+    return false;
+  }
   if (force) {
     await git.reset("hard");
     return git.pull();
