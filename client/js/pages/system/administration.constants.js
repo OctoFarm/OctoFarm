6587bd12c906fd8367ba01c1ad7b8061564dfe25
James <james@vexica.com>
2022-06-13 14:29:44 +0100

    chore(client): finalise administration page layout and settings

--- client/js/pages/system/administration.constants.js
+++ client/js/pages/system/administration.constants.js
@@ -2,6 +2,23 @@ export const MENU_IDS = {
     adminTasks: "admin-tasks-list"
 }
 export const SAVE_BTN_IDS = {
+    saveSystem: "save-system-changes",
+    saveTheme: "save-theme-changes",
+    savePrinters: "save-printer-changes",
+    saveViews: "save-views-changes",
+    saveHistory: "save-history-changes",
+    saveFilament: "save-filament-changes",
+    setupFilamentManager: "filament-manager-plugin-action",
+    saveInflux: "save-influx-changes"
+}
 
+export const UPDATE_BTN_IDS = {
+    checkUpdates: "check-updates",
+    updateOctoFarm: "update-octofarm"
+}
+
+export const SETUP_OCTOPRINT_BTN_IDS = {
+    setupFilamentManager: "setup-op-filament-manager",
+    setupHistorySnapshot: "setup-op-history-snapshot",
 }
 
