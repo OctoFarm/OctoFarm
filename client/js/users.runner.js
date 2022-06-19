99d2d6845cc0c30d129964a962d272e2f1ac375f
NotExpectedYet <online@notexpectedyet.com>
2022-06-19 06:47:49 +0100

    feat(client): new user manager page

---
+++ client/js/users.runner.js
@@ -0,0 +1,43 @@
+// Setup user edits
+import { returnSaveBtn, userActionElements } from "./pages/system/server.options";
+import {deleteUser, editUser, fillInEditInformation} from "./pages/system/server.actions";
+
+const userEditButtons = document.querySelectorAll("*[id^=\"editUserBtn-\"]");
+userEditButtons.forEach((btn) => {
+    const split = btn.id.split("-");
+    btn.addEventListener("click", async (e) => {
+        userActionElements.editUserFooter.innerHTML = `
+    ${returnSaveBtn()}
+  `;
+        await fillInEditInformation(split[1]);
+        const userActionSave = document.getElementById("userActionSave");
+        userActionSave.addEventListener("click", async () => {
+            await editUser(split[1]);
+        });
+    });
+});
+
+// Setup user deletes
+const deleteButtons = document.querySelectorAll("*[id^=\"deleteUserBtn-\"]");
+deleteButtons.forEach((btn) => {
+    const split = btn.id.split("-");
+    btn.addEventListener("click", (e) => {
+        deleteUser(split[1]);
+    });
+});
+
+$("#userCreateModal").on("hidden.bs.modal", function (e) {
+    const userActionSave = document.getElementById("userActionSave");
+    userActionElements.userCreateMessage.innerHTML = "";
+    userActionSave.remove();
+});
+$("#usersResetPasswordModal").on("hidden.bs.modal", function (e) {
+    const userActionSave = document.getElementById("userActionSave");
+    userActionElements.userResetMessage.innerHTML = "";
+    userActionSave.remove();
+});
+$("#userEditModal").on("hidden.bs.modal", function (e) {
+    const userActionSave = document.getElementById("userActionSave");
+    userActionElements.userEditMessage.innerHTML = "";
+    userActionSave.remove();
+});
