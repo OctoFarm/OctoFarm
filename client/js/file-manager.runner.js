992b2656f9152720e82ca705f9bd43293c9fa780
notexpectedyet <online@notexpectedyet.com>
2022-06-22 06:46:08 +0100

    fix(client): fix storage used warning not using storage used for calculation closes #1108

--- client/js/file-manager.runner.js
+++ client/js/file-manager.runner.js
@@ -54,19 +54,20 @@ class Manager {
     for (const [index, printer] of onlinePrinterList.entries()) {
       let storageWarning = "";
       if (printer?.storage) {
+        const storageUsed = printer.storage.total - printer.storage.free;
         const percentRemain =
-          (printer.storage.free * 100) / printer.storage.total;
-        if (percentRemain > 90) {
+          (storageUsed * 100) / printer.storage.total;
+        if (percentRemain <= 10) {
           storageWarning = `<button type="button" class="btn btn-outline-danger text-left disabled btn-sm"
                                    style="pointer-events: none" disabled>${percentRemain.toFixed(
                                      0
-                                   )}% Space Left</button>`;
+                                   )}% Space Remains</button>`;
         }
-        if (percentRemain > 80) {
+        if (percentRemain <= 20) {
           storageWarning = `<button type="button" class="btn btn-outline-warning text-left disabled btn-sm"
                                    style="pointer-events: none" disabled>${percentRemain.toFixed(
                                      0
-                                   )}% Space Left</button>`;
+                                   )}% Space Remains</button>`;
         }
       }
 
