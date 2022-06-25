20a40a1b80a7eb5129e5921366ba5d0f0e209228
notexpectedyet <online@notexpectedyet.com>
2022-06-25 08:20:22 +0100

    feat(client): show current OctoPrint and Active Control users on Printer Manager

--- client/js/pages/printer-manager/templates/printer-table-row.templates.js
+++ client/js/pages/printer-manager/templates/printer-table-row.templates.js
@@ -231,6 +231,9 @@ export function returnPrinterTableRow(printer) {
         <td class="align-middle">
             <span id="printerURL-${printer._id}"></span>
         </td>
+        <td class="align-middle">
+            <span id="printerOctoPrintUser-${printer._id}"></span>
+        </td>
                 <td class="align-middle">
             <small>
                 <span data-title="${printer.hostState.desc}" id="hostBadge-${
@@ -262,11 +265,14 @@ export function returnPrinterTableRow(printer) {
                 </span>
             </small>
         </td>
+
         <td class="align-middle" id="printerGroup-${printer._id}"></td>
-                <td class="align-middle" id="printerActionBtns-${printer._id}">
+         <td class="align-middle">
+            <span id="printerControlUser-${printer._id}"></span>
+        </td>
+        <td class="align-middle" id="printerActionBtns-${printer._id}">
 
         </td>
-
         <td class="align-middle" id="printerManageBtns-${printer._id}">
             ${returnPrinterManageDropDown(printer._id, printer.disabled)}
         </td>
