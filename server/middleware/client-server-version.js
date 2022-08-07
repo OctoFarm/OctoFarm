e59a040205e2d1380010a110e206c978e9a301f0
notexpectedyet <online@notexpectedyet.com>
2022-08-07 08:24:15 +0100

    chore(main): replace usage of client version across the app

--- server/middleware/client-server-version.js
+++ server/middleware/client-server-version.js
@@ -1,10 +1,9 @@
 const { AppConstants } = require("../constants/app.constants");
-const { fetchClientVersion } = require("../app-env");
 
 module.exports = {
   ensureClientServerVersion(_req, res, next) {
     res.locals.serverVersion = process.env[AppConstants.VERSION_KEY];
-    res.locals.clientVersion = fetchClientVersion();
+    res.locals.clientVersion = process.env[AppConstants.VERSION_KEY];
     next();
   }
 };
