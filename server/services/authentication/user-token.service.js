9684c726f18147390b8a5b25bbc2d8feb5696fa4
James <james@vexica.com>
2022-10-04 10:42:43 +0100

    refactor(server): remove duplicated utilities

--- server/services/authentication/user-token.service.js
+++ server/services/authentication/user-token.service.js
@@ -1,19 +1,4 @@
-function randomString(len) {
-  const buf = [],
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-    charlen = chars.length;
-
-  for (let i = 0; i < len; ++i) {
-    buf.push(chars[getRandomInt(0, charlen - 1)]);
-  }
-
-  return buf.join("");
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+const { randomString } = require("../../utils/random.util")
 class UserTokenService {
   static tokens = {};
 
