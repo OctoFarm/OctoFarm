010601576d17ade979e50b1e49342cf09c9f4a05
James <james@vexica.com>
2022-10-06 14:55:59 +0100

    refactor(docker): fix incorrect log path for docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -18,4 +18,4 @@ else
     echo "Logs folder already exists..."
 fi
 
-cd server && pm2 start app.js --name OctoFarm --no-daemon -o 'logs/pm2.log' -e 'logs/pm2.error.log' --time  --restart-delay=1000 --exp-backoff-restart-delay=1500
+cd server && pm2 start app.js --name OctoFarm --no-daemon -o '../logs/pm2.log' -e '../logs/pm2.error.log' --time  --restart-delay=1000 --exp-backoff-restart-delay=1500
