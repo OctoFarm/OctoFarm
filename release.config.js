1f685aff863e517499c6f38a9223f830a6a1aea6
notexpectedyet <online@notexpectedyet.com>
2022-10-15 10:51:43 +0100

    chore(main): generate and package up /server directory into zip for release

--- release.config.js
+++ release.config.js
@@ -91,7 +91,7 @@ module.exports = {
       '@semantic-release/exec',
       {
         // eslint-disable-next-line no-template-curly-in-string
-        prepareCmd: 'VERSION=${nextRelease.version} npm run bump && cd client && npm run build && cd ../',
+        prepareCmd: 'VERSION=${nextRelease.version} npm run bump && cd client && npm run build && cd ../ && chmod +x scripts/create-release-zip.sh',
       },
     ],
     [
