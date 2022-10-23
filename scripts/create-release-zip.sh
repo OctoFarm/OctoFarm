8fc9b1533d08542b1883609625ff5779e762976d
notexpectedyet <online@notexpectedyet.com>
2022-10-23 07:28:02 +0100

    refactor(main): add version stub to packaged zip

--- scripts/create-release-zip.sh
+++ scripts/create-release-zip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PACKAGE_VERSION=$(cat package.json|grep version|head -1|awk -F: '{ print $2 }'|sed 's/[", ]//g')
 
-echo "OctoFarm Packaging: creating a release zip at path: $PWD/octofarm.zip"
+echo "OctoFarm Packaging Version: creating a release zip at path: $PWD/octofarm-${PACKAGE_VERSION}.zip"
 
-zip -r octofarm.zip server
+zip -r "octofarm-${PACKAGE_VERSION}.zip" server -x server/node_modules/**\*
 
-echo "OctoFarm Packaging: done creating release $PWD/octofarm.zip package!"
+echo "OctoFarm Packaging: done creating release $PWD/octofarm${PACKAGE_VERSION}.zip package!"
