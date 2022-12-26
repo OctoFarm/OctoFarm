663285d5cc5f57814cadfc6f65e887f127c5075c
notexpectedyet <online@notexpectedyet.com>
2022-12-26 09:01:48 +0000

    ci(main): update patreons daily and store back to cache in git repository

---
+++ SPONSORS.md
@@ -0,0 +1,19 @@
+### Patreons #### 
+- Guy Barton 
+- EIPS  
+- Will Nelson 
+- Scott Windmiller 
+- Curtis Brooks 
+- Chadd Giesing 
+- Josheua R Mascote 
+- Tideline3D 
+- geekfisher 
+- Nick Darrow 
+- N.R 
+- Mohammad Mansour 
+- Feldherr 
+- Pat Davidse 
+- David Z 
+- David Peters 
+- Joey Cortez 
+- Sven Meendermann 
--- server/services/patreon.service.js
+++ server/services/patreon.service.js
@@ -1,5 +1,4 @@
-const fetch = require("node-fetch");
-let patreonData = require("../constants/patreon.constants");
+const patreonData = require("../constants/patreon.constants");
 const Logger = require("../handlers/logger");
 const { LOGGER_ROUTE_KEYS } = require("../constants/logger.constants");
 const logger = new Logger(LOGGER_ROUTE_KEYS.SERVICE_PATREON);
@@ -8,21 +7,6 @@ function returnPatreonData() {
   return patreonData;
 }
 
-async function grabLatestPatreonData() {
-  const latestPatreonData = await fetch("https://api.octofarm.net/statistics", {
-    method: "GET"
-  });
-  if (latestPatreonData.status === 200) {
-    const getJSON = await latestPatreonData.json();
-    patreonData = getJSON.patreons.applicationPledges;
-
-    logger.debug("Successfully grabbed remote patreon data!");
-  } else {
-    logger.error("Falling back to local patreon data...");
-  }
-}
-
 module.exports = {
-  returnPatreonData,
-  grabLatestPatreonData
+  returnPatreonData
 };
