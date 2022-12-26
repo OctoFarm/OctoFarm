7c2a6b0dbb4364e66b836bcbca8cf66b82d0a3b2
notexpectedyet <online@notexpectedyet.com>
2022-12-26 07:13:49 +0000

    removed(docker): monolithic docker container

--- scripts/run-build-sequence.sh
+++ scripts/run-build-sequence.sh
@@ -1,9 +1,9 @@
-echo "Running build sequence for Version: $VERSION"
+echo "Running build sequence for Version: $1"
 ## BUILD CLIENT FROM SOURCE FILES
 chmod +x scripts/build-client-from-source.sh
 scripts/build-client-from-source.sh
 ### CREATE RELEASE ZIP
-chmod +x scripts/create-release-zip.sh $VERSION
+chmod +x scripts/create-release-zip.sh $1
 scripts/create-release-zip.sh
 ## BUILD DOCKER IMAGES
 chmod +x scripts/build-docker-images.sh
